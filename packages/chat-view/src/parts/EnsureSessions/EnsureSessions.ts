9f21d4e738bf5876545ecc5ca46cc3dbb9a76591
Le Vivilet <levivilet@pm.me>
2026-02-27 18:41:17 +0100

    fix

--- packages/chat-view/src/parts/EnsureSessions/EnsureSessions.ts
+++ packages/chat-view/src/parts/EnsureSessions/EnsureSessions.ts
@@ -2,8 +2,5 @@ import type { ChatSession } from '../StatusBarState/StatusBarState.ts'
 import type { ChatState } from '../StatusBarState/StatusBarState.ts'
 
 export const ensureSessions = (state: ChatState): readonly ChatSession[] => {
-  if (state.sessions.length > 0) {
-    return state.sessions
-  }
-  return []
+  return state.sessions
 }
