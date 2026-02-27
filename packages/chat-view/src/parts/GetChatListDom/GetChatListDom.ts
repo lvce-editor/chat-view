ff6700bc32b5e258ca1c4a1e9a48d2edbeda2572
Le Vivilet <levivilet@pm.me>
2026-02-27 18:17:08 +0100

    move

---
+++ packages/chat-view/src/parts/GetChatListDom/GetChatListDom.ts
@@ -0,0 +1,18 @@
+import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { ChatSession } from '../StatusBarState/StatusBarState.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { getEmptyChatSessionsDom } from '../GetChatViewDom/GetEmptyChatSessionsDom.ts'
+import { getSessionDom } from '../GetChatViewDom/GetSessionDom.ts'
+
+export const getChatListDom = (sessions: readonly ChatSession[], selectedSessionId: string): readonly VirtualDomNode[] => {
+  const sessionNodes = sessions.flatMap((session) => getSessionDom(session, selectedSessionId))
+  const emptyStateNodes = getEmptyChatSessionsDom(sessions.length)
+  return [
+    {
+      childCount: sessions.length === 0 ? 1 : sessions.length,
+      className: ClassNames.ChatList,
+      type: VirtualDomElements.Div,
+    },
+    ...(sessions.length === 0 ? emptyStateNodes : sessionNodes),
+  ]
+}
--- packages/chat-view/src/parts/GetChatModeListVirtualDom/GetChatModeListVirtualDom.ts
+++ packages/chat-view/src/parts/GetChatModeListVirtualDom/GetChatModeListVirtualDom.ts
@@ -1,23 +1,8 @@
 import { type VirtualDomNode, mergeClassNames, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { ChatSession } from '../StatusBarState/StatusBarState.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getChatHeaderListModeDom } from '../GetChatHeaderDomListMode/GetChatHeaderDomListMode.ts'
-import { getEmptyChatSessionsDom } from '../GetChatViewDom/GetEmptyChatSessionsDom.ts'
-import { getSessionDom } from '../GetChatViewDom/GetSessionDom.ts'
-
-export const getChatListDom = (sessions: readonly ChatSession[], selectedSessionId: string): readonly VirtualDomNode[] => {
-  const sessionNodes = sessions.flatMap((session) => getSessionDom(session, selectedSessionId))
-  const emptyStateNodes = getEmptyChatSessionsDom(sessions.length)
-  return [
-    {
-      childCount: sessions.length === 0 ? 1 : sessions.length,
-      className: ClassNames.ChatList,
-      type: VirtualDomElements.Div,
-    },
-    ...(sessions.length === 0 ? emptyStateNodes : sessionNodes),
-  ]
-}
+import { getChatListDom } from '../GetChatListDom/GetChatListDom.ts'
 
 export const getChatModeListVirtualDom = (sessions: readonly ChatSession[], selectedSessionId: string): readonly VirtualDomNode[] => {
   return [
