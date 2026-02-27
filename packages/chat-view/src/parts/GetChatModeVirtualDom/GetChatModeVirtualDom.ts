30dbcaf0e4f372f037f4f94d94827317be7e7d36
Le Vivilet <levivilet@pm.me>
2026-02-27 18:04:20 +0100

    move

--- packages/chat-view/src/parts/GetChatModeVirtualDom/GetChatModeVirtualDom.ts
+++ packages/chat-view/src/parts/GetChatModeVirtualDom/GetChatModeVirtualDom.ts
@@ -1,21 +1,35 @@
-import { type VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { ChatMessage } from '../StatusBarState/StatusBarState.ts'
 import type { ChatSession } from '../StatusBarState/StatusBarState.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getChatContentDom } from '../GetChatViewDom/GetChatContentDom.ts'
+import { getChatHeaderActionsDom } from '../GetChatViewDom/GetChatHeaderActionsDom.ts'
+import { getChatHeaderDom } from '../GetChatViewDom/GetChatHeaderDom.ts'
 import * as Strings from '../GetChatViewDom/GetChatViewDomStrings.ts'
 import { getEmptyChatSessionsDom } from '../GetChatViewDom/GetEmptyChatSessionsDom.ts'
 import { getMessagesDom } from '../GetChatViewDom/GetMessagesDom.ts'
 import { getSessionDom } from '../GetChatViewDom/GetSessionDom.ts'
 
-export const getChatModeListVirtualDom = (
-  sessions: readonly ChatSession[],
-  selectedSessionId: string,
-): readonly VirtualDomNode[] => {
+export const getChatModeListVirtualDom = (sessions: readonly ChatSession[], selectedSessionId: string): readonly VirtualDomNode[] => {
   const selectedSession = sessions.find((session) => session.id === selectedSessionId)
   const selectedSessionTitle = selectedSession?.title || Strings.chatTitle
   const sessionNodes = sessions.flatMap((session) => getSessionDom(session, selectedSessionId))
   const emptyStateNodes = getEmptyChatSessionsDom(sessions.length)
-  return getChatContentDom('list', sessions.length, emptyStateNodes, sessionNodes, selectedSessionTitle, [], '')
+  const contentNodes = getChatContentDom('list', sessions.length, emptyStateNodes, sessionNodes, selectedSessionTitle, [], '')
+  return [
+    {
+      childCount: 2,
+      className: ClassNames.Viewlet + ' Chat',
+      onClick: DomEventListenerFunctions.HandleClick,
+      onInput: DomEventListenerFunctions.HandleInput,
+      onKeyDown: DomEventListenerFunctions.HandleKeyDown,
+      type: VirtualDomElements.Div,
+    },
+    ...getChatHeaderDom(false, selectedSessionTitle),
+    ...getChatHeaderActionsDom(),
+    ...contentNodes,
+  ]
 }
 
 export const getChatModeDetailVirtualDom = (
@@ -27,5 +41,18 @@ export const getChatModeDetailVirtualDom = (
   const selectedSessionTitle = selectedSession?.title || Strings.chatTitle
   const messages: readonly ChatMessage[] = selectedSession ? selectedSession.messages : []
   const messagesNodes = getMessagesDom(messages)
-  return getChatContentDom('detail', sessions.length, [], [], selectedSessionTitle, messagesNodes, composerValue)
+  const contentNodes = getChatContentDom('detail', sessions.length, [], [], selectedSessionTitle, messagesNodes, composerValue)
+  return [
+    {
+      childCount: 2,
+      className: ClassNames.Viewlet + ' Chat',
+      onClick: DomEventListenerFunctions.HandleClick,
+      onInput: DomEventListenerFunctions.HandleInput,
+      onKeyDown: DomEventListenerFunctions.HandleKeyDown,
+      type: VirtualDomElements.Div,
+    },
+    ...getChatHeaderDom(true, selectedSessionTitle),
+    ...getChatHeaderActionsDom(),
+    ...contentNodes,
+  ]
 }
--- packages/chat-view/src/parts/GetChatViewDom/GetChatViewDom.ts
+++ packages/chat-view/src/parts/GetChatViewDom/GetChatViewDom.ts
@@ -1,38 +1,14 @@
-import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { ChatViewMode } from '../ChatViewMode/ChatViewMode.ts'
 import type { ChatSession } from '../StatusBarState/StatusBarState.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getChatModeDetailVirtualDom, getChatModeListVirtualDom } from '../GetChatModeVirtualDom/GetChatModeVirtualDom.ts'
-import { getChatHeaderActionsDom } from './GetChatHeaderActionsDom.ts'
-import { getChatHeaderDom } from './GetChatHeaderDom.ts'
-import * as Strings from './GetChatViewDomStrings.ts'
 
 export const getChatVirtualDom = (
   sessions: readonly ChatSession[],
   selectedSessionId: string,
   composerValue: string,
   viewMode: ChatViewMode,
-): readonly VirtualDomNode[] => {
-  const selectedSession = sessions.find((session) => session.id === selectedSessionId)
-  const selectedSessionTitle = selectedSession?.title || Strings.chatTitle
-  const showBackButton = viewMode === 'detail'
-  const contentNodes =
-    viewMode === 'detail'
-      ? getChatModeDetailVirtualDom(sessions, selectedSessionId, composerValue)
-      : getChatModeListVirtualDom(sessions, selectedSessionId)
-  const dom: VirtualDomNode[] = [
-    {
-      childCount: 2,
-      className: ClassNames.Viewlet + ' Chat',
-      onClick: DomEventListenerFunctions.HandleClick,
-      onInput: DomEventListenerFunctions.HandleInput,
-      onKeyDown: DomEventListenerFunctions.HandleKeyDown,
-      type: VirtualDomElements.Div,
-    },
-    ...getChatHeaderDom(showBackButton, selectedSessionTitle),
-    ...getChatHeaderActionsDom(),
-    ...contentNodes,
-  ]
-  return dom
-}
+): readonly VirtualDomNode[] =>
+  viewMode === 'detail'
+    ? getChatModeDetailVirtualDom(sessions, selectedSessionId, composerValue)
+    : getChatModeListVirtualDom(sessions, selectedSessionId)
