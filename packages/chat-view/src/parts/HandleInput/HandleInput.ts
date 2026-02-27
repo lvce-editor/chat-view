4cbc8125beb60a020f30d03c4fb422c211c90df5
Le Vivilet <levivilet@pm.me>
2026-02-27 21:51:27 +0100

    log

--- packages/chat-view/src/parts/HandleInput/HandleInput.ts
+++ packages/chat-view/src/parts/HandleInput/HandleInput.ts
@@ -1,7 +1,6 @@
 import type { ChatState } from '../StatusBarState/StatusBarState.ts'
 
 export const handleInput = async (state: ChatState, value: string, inputSource: 'user' | 'script' = 'user'): Promise<ChatState> => {
-  console.log({ inputSource, value })
   if (state.ignoreNextInput) {
     return {
       ...state,
