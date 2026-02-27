a940ea3148cf48b9e30e8ec463fcbca3098f2211
Le Vivilet <levivilet@pm.me>
2026-02-27 18:33:21 +0100

    move

--- packages/chat-view/src/parts/GetChatListDom/GetChatListDom.ts
+++ packages/chat-view/src/parts/GetChatListDom/GetChatListDom.ts
@@ -1,8 +1,8 @@
 import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { ChatSession } from '../StatusBarState/StatusBarState.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getEmptyChatSessionsDom } from '../GetEmptyChatSessionsDom/GetEmptyChatSessionsDom.ts'
 import { getSessionDom } from '../GetSessionDom/GetSessionDom.ts'
-import type { ChatSession } from '../StatusBarState/StatusBarState.ts'
 
 export const getChatListDom = (sessions: readonly ChatSession[], selectedSessionId: string): readonly VirtualDomNode[] => {
   if (sessions.length === 0) {
