7cc1a09de7c73043bf91abd7f3b31121d8d0fa8c
Le Vivilet <levivilet@pm.me>
2026-02-27 22:24:51 +0100

    fix

--- packages/e2e/src/chat-view.create-session-behavior.ts
+++ packages/e2e/src/chat-view.create-session-behavior.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.create-session-behavior'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, Locator }) => {
   await Command.execute('Chat.setChatList', 1)
 
