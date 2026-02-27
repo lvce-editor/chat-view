b24e4fa0cf5c9a8b2e24dd288b24fdc0437f9a0b
Le Vivilet <levivilet@pm.me>
2026-02-27 15:57:55 +0100

    skip

--- packages/e2e/src/chat-view.chat-list.ts
+++ packages/e2e/src/chat-view.chat-list.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.chat-list'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ Command, expect, Locator }) => {
   await Command.execute('Chat.setChatList', 1)
