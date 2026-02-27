fad72234ff2d0f27c285bcd985442c1fe5cb3b62
Le Vivilet <levivilet@pm.me>
2026-02-27 21:59:32 +0100

    test

--- packages/e2e/src/chat-view.handle-input-script.ts
+++ packages/e2e/src/chat-view.handle-input-script.ts
@@ -5,12 +5,14 @@ export const name = 'chat-view.handle-input-script'
 export const skip = 1
 
 export const test: Test = async ({ Command, expect, Locator }) => {
-  await Command.execute('Chat.setChatList', 1)
-
+  // arrange
+  await Command.execute('Layout.showSecondarySideBar')
   const composer = Locator('.MultilineInputBox[name="composer"]')
   await expect(composer).toBeVisible()
 
+  // act
   await Command.execute('Chat.handleInput', 'hello from script input', 'script')
 
+  // assert
   await expect(composer).toHaveValue('hello from script input')
 }
