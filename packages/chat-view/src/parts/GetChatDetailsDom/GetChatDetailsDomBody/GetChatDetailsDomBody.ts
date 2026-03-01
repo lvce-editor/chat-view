634ff50785a8afcd14a5a85527ec970d0f88992d
Le Vivilet <levivilet@pm.me>
2026-03-01 09:52:18 +0100

    feature: refactor chat details DOM structure and add utility functions for token usage overview (#60)

---
+++ packages/chat-view/src/parts/GetChatDetailsDom/GetChatDetailsDomBody/GetChatDetailsDomBody.ts
@@ -0,0 +1,31 @@
+import { type VirtualDomNode, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
+import type { ChatModel } from '../../ChatModel/ChatModel.ts'
+import * as ClassNames from '../../ClassNames/ClassNames.ts'
+import { getChatSendAreaDom } from '../GetChatSendAreaDom/GetChatSendAreaDom.ts'
+
+export const getChatDetailsDom = (
+  selectedSessionTitle: string,
+  messagesNodes: readonly VirtualDomNode[],
+  composerValue: string,
+  models: readonly ChatModel[],
+  selectedModelId: string,
+  usageOverviewEnabled: boolean,
+  tokensUsed: number,
+  tokensMax: number,
+): readonly VirtualDomNode[] => {
+  return [
+    {
+      childCount: 3,
+      className: ClassNames.ChatDetails,
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 1,
+      className: ClassNames.Label,
+      type: VirtualDomElements.Span,
+    },
+    text(selectedSessionTitle),
+
+    ...getChatSendAreaDom(composerValue, models, selectedModelId, usageOverviewEnabled, tokensUsed, tokensMax),
+  ]
+}
