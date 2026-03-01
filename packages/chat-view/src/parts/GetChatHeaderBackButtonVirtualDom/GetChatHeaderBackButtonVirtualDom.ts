737fe0ead08fbbfe96ba07176a962f82094f16d1
Le Vivilet <levivilet@pm.me>
2026-03-01 09:46:02 +0100

    fix

--- packages/chat-view/src/parts/GetChatHeaderBackButtonVirtualDom/GetChatHeaderBackButtonVirtualDom.ts
+++ packages/chat-view/src/parts/GetChatHeaderBackButtonVirtualDom/GetChatHeaderBackButtonVirtualDom.ts
@@ -1,4 +1,4 @@
-import { type VirtualDomNode, AriaRoles, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
+import { type VirtualDomNode, AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as Strings from '../GetChatViewDomStrings/GetChatViewDomStrings.ts'
