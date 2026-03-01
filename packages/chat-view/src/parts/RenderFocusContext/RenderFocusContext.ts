c2ea4263cb30dc8be7a4d6163a5ee4325dfe4e49
Le Vivilet <levivilet@pm.me>
2026-03-01 09:39:58 +0100

    when

--- packages/chat-view/src/parts/RenderFocusContext/RenderFocusContext.ts
+++ packages/chat-view/src/parts/RenderFocusContext/RenderFocusContext.ts
@@ -1,7 +1,6 @@
-import { ViewletCommand } from '@lvce-editor/constants'
+import { ViewletCommand, WhenExpression } from '@lvce-editor/constants'
 import type { ChatState } from '../ChatState/ChatState.ts'
 
 export const renderFocusContext = (oldState: ChatState, newState: ChatState): readonly any[] => {
-  const when = 2344
-  return [ViewletCommand.SetFocusContext, newState.uid, when]
+  return [ViewletCommand.SetFocusContext, newState.uid, WhenExpression.FocusChatInput]
 }
