6be32c965fcff0d6cc596d898d3539bd6a1bd0cd
Le Vivilet <levivilet@pm.me>
2026-03-01 21:50:22 +0100

    fix

--- packages/chat-view/test/Create.test.ts
+++ packages/chat-view/test/Create.test.ts
@@ -5,7 +5,7 @@ import * as StatusBarStates from '../src/parts/StatusBarStates/StatusBarStates.t
 
 test('create should store state with the given uid', () => {
   const uid = 123
-  Create.create(uid, '', 10, 20, 300, 400, 0, '')
+  Create.create(uid, 10, 20, 300, 400, 0, '')
   const result = StatusBarStates.get(uid)
   const { newState } = result
   const newStateTyped: ChatState = newState
