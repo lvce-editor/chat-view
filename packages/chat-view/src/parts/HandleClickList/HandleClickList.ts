ed69aee0da58232b35deca201f861a3aaafcd9e7
Le Vivilet <levivilet@pm.me>
2026-03-01 21:48:24 +0100

    feature: refactor handleClickList to use selectListIndex for improved session selection (#71)

--- packages/chat-view/src/parts/HandleClickList/HandleClickList.ts
+++ packages/chat-view/src/parts/HandleClickList/HandleClickList.ts
@@ -1,16 +1,8 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
 import { getListIndex } from '../GetListIndex/GetListIndex.ts'
-import { selectSession } from '../SelectSession/SelectSession.ts'
+import { selectListIndex } from '../SelectListIndex/SelectListIndex.ts'
 
 export const handleClickList = async (state: ChatState, eventX: number, eventY: number): Promise<ChatState> => {
-  const { sessions } = state
   const index = getListIndex(state, eventX, eventY)
-  if (index < 0) {
-    return state
-  }
-  const session = sessions[index]
-  if (!session) {
-    return state
-  }
-  return selectSession(state, session.id)
+  return selectListIndex(state, index)
 }
