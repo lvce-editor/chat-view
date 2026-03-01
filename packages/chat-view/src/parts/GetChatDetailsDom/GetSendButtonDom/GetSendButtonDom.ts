aa018faffc12afbfef7f65e31ee1df7edb19e074
Le Vivilet <levivilet@pm.me>
2026-03-01 21:33:25 +0100

    feature: refactor getSendButtonClassName to a separate module (#68)

--- packages/chat-view/src/parts/GetChatDetailsDom/GetSendButtonDom/GetSendButtonDom.ts
+++ packages/chat-view/src/parts/GetChatDetailsDom/GetSendButtonDom/GetSendButtonDom.ts
@@ -1,13 +1,7 @@
 import { type VirtualDomNode, AriaRoles, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
-import * as ClassNames from '../../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as Strings from '../../GetChatViewDomStrings/GetChatViewDomStrings.ts'
-
-export const getSendButtonClassName = (isSendDisabled: boolean): string => {
-  return isSendDisabled
-    ? `${ClassNames.Button} ${ClassNames.ButtonPrimary} ${ClassNames.ButtonDisabled}`
-    : `${ClassNames.Button} ${ClassNames.ButtonPrimary}`
-}
+import { getSendButtonClassName } from '../GetSendButtonClassName/GetSendButtonClassName.ts'
 
 export const getSendButtonDom = (isSendDisabled: boolean): readonly VirtualDomNode[] => {
   const sendButtonClassName = getSendButtonClassName(isSendDisabled)
