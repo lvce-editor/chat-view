9cd97e29471962920699f4599b4372239e3426ad
Le Vivilet <levivilet@pm.me>
2026-03-02 22:36:35 +0100

    fix

---
+++ packages/chat-view/src/parts/GetEmptyMessagesDom/GetEmptyMessagesDom.ts
@@ -0,0 +1,14 @@
+import { type VirtualDomNode, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as Strings from '../GetChatViewDomStrings/GetChatViewDomStrings.ts'
+
+export const getEmptyMessagesDom = (): readonly VirtualDomNode[] => {
+  return [
+    {
+      childCount: 1,
+      className: ClassNames.ChatWelcomeMessage,
+      type: VirtualDomElements.Div,
+    },
+    text(Strings.startConversation()),
+  ]
+}
--- packages/chat-view/src/parts/GetMessagesDom/GetMessagesDom.ts
+++ packages/chat-view/src/parts/GetMessagesDom/GetMessagesDom.ts
@@ -1,19 +1,7 @@
-import { type VirtualDomNode, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
+import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { ChatMessage } from '../ChatState/ChatState.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetChatMessageDom from '../GetChatMessageDom/GetChatMessageDom.ts'
-import * as Strings from '../GetChatViewDomStrings/GetChatViewDomStrings.ts'
-
-export const getEmptyMessagesDom = (): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.ChatWelcomeMessage,
-      type: VirtualDomElements.Div,
-    },
-    text(Strings.startConversation()),
-  ]
-}
+import * as GetEmptyMessagesDom from '../GetEmptyMessagesDom/GetEmptyMessagesDom.ts'
 
 export const getMessagesDom = (
   messages: readonly ChatMessage[],
@@ -21,7 +9,7 @@ export const getMessagesDom = (
   openApiApiKeyInput = '',
 ): readonly VirtualDomNode[] => {
   if (messages.length === 0) {
-    return getEmptyMessagesDom()
+    return GetEmptyMessagesDom.getEmptyMessagesDom()
   }
   return [
     {
