0824690c9e888d798b04051a4d1f2f1d55501470
Le Vivilet <levivilet@pm.me>
2026-03-07 22:22:45 +0100

    fix

--- packages/chat-view/src/parts/RenderScrollTop/RenderScrollTop.ts
+++ packages/chat-view/src/parts/RenderScrollTop/RenderScrollTop.ts
@@ -2,8 +2,5 @@ import { ViewletCommand } from '@lvce-editor/constants'
 import type { ChatState } from '../ChatState/ChatState.ts'
 
 export const renderScrollTop = (oldState: ChatState, newState: ChatState): any => {
-  if (patches.length === 0) {
-    return []
-  }
   return [ViewletCommand.SetProperty, newState.uid, '.ChatList', 'scrollTop', newState.messagesScrollTop]
 }
