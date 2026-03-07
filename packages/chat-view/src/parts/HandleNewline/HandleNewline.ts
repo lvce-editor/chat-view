52cc2a8d37c852f145ea48425cd0e065be09901c
Le Vivilet <levivilet@pm.me>
2026-03-07 15:48:20 +0100

    refactor: simplify handleNewline by destructuring composerValue from state

--- packages/chat-view/src/parts/HandleNewline/HandleNewline.ts
+++ packages/chat-view/src/parts/HandleNewline/HandleNewline.ts
@@ -3,5 +3,6 @@ import * as HandleInput from '../HandleInput/HandleInput.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const handleNewline = async (state: ChatState): Promise<ChatState> => {
-  return HandleInput.handleInput(state, InputName.Composer, `${state.composerValue}\n`)
+  const { composerValue } = state
+  return HandleInput.handleInput(state, InputName.Composer, `${composerValue}\n`)
 }
