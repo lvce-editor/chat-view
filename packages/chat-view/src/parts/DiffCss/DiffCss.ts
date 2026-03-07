5658d07ae6d8488eb47d7b08283dd4e72ac33f40
Le Vivilet <levivilet@pm.me>
2026-03-07 15:51:10 +0100

    feature: enhance isEqual function to compare additional ChatState properties (#116)

--- packages/chat-view/src/parts/DiffCss/DiffCss.ts
+++ packages/chat-view/src/parts/DiffCss/DiffCss.ts
@@ -1,5 +1,11 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
 
 export const isEqual = (oldState: ChatState, newState: ChatState): boolean => {
-  return oldState.initial === newState.initial
+  return (
+    oldState.initial === newState.initial &&
+    oldState.composerHeight === newState.composerHeight &&
+    oldState.composerLineHeight === newState.composerLineHeight &&
+    oldState.composerFontFamily === newState.composerFontFamily &&
+    oldState.composerFontSize === newState.composerFontSize
+  )
 }
