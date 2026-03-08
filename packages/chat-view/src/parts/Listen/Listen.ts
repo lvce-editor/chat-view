aea5c90c48a2ee50b1d9bf43b1475fd4d7585266
Le Vivilet <levivilet@pm.me>
2026-03-08 11:21:27 +0100

    feature: add initializeChatNetworkWorker function for message port handling (#145)

--- packages/chat-view/src/parts/Listen/Listen.ts
+++ packages/chat-view/src/parts/Listen/Listen.ts
@@ -1,6 +1,7 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import { initializeChatNetworkWorker } from '../InitializeChatNetworkWorker/InitializeChatNetworkWorker.ts'
 import { registerCommands } from '../StatusBarStates/StatusBarStates.ts'
 
 export const listen = async (): Promise<void> => {
@@ -9,4 +10,5 @@ export const listen = async (): Promise<void> => {
     commandMap: CommandMap.commandMap,
   })
   RendererWorker.set(rpc)
+  await initializeChatNetworkWorker()
 }
