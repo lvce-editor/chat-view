f8de1ae2a4e2d9b1deaf17aec85b33bb562ec0a3
Le Vivilet <levivilet@pm.me>
2026-03-08 19:24:06 +0100

    fix

--- packages/chat-view/src/parts/GetToolCallDom/GetToolCallDom.ts
+++ packages/chat-view/src/parts/GetToolCallDom/GetToolCallDom.ts
@@ -2,7 +2,7 @@ import { type VirtualDomNode, VirtualDomElements, text } from '@lvce-editor/virt
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getToolCallArgumentPreview } from '../GetToolCallArgumentPreview/GetToolCallArgumentPreview.ts'
 
-export const getToolCallDom = (toolCall: { name: string; arguments: string }): readonly VirtualDomNode[] => {
+export const getToolCallDom = (toolCall: { readonly name: string; readonly arguments: string }): readonly VirtualDomNode[] => {
   const argumentPreview = getToolCallArgumentPreview(toolCall.arguments)
   const label = `${toolCall.name} ${argumentPreview}`
   return [
