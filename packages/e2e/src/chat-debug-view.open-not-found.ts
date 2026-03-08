dc0870a33b0b40ae470657ea1d19d690929d106e
Le Vivilet <levivilet@pm.me>
2026-03-08 17:27:23 +0100

    skip

--- packages/e2e/src/chat-debug-view.open-not-found.ts
+++ packages/e2e/src/chat-debug-view.open-not-found.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-debug-view.open-not-found'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, Locator }) => {
   // act
   await Command.execute('Main.openUri', 'chat-debug://not-found')
