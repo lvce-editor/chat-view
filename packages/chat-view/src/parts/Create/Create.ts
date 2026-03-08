b24b017a9ab8903e87d41679c224b28a27d6db0d
Le Vivilet <levivilet@pm.me>
2026-03-08 17:44:31 +0100

    fix

--- packages/chat-view/src/parts/Create/Create.ts
+++ packages/chat-view/src/parts/Create/Create.ts
@@ -2,14 +2,13 @@ import type { ChatState } from '../ChatState/ChatState.ts'
 import { createDefaultState } from '../CreateDefaultState/CreateDefaultState.ts'
 import { set } from '../StatusBarStates/StatusBarStates.ts'
 
-export const create = (uid: number, uri: string, x: number, y: number, width: number, height: number, platform: number, assetDir: string): void => {
+export const create = (uid: number, x: number, y: number, width: number, height: number, platform: number, assetDir: string): void => {
   const state: ChatState = {
     ...createDefaultState(),
     assetDir,
     height,
     platform,
     uid,
-    uri,
     width,
     x,
     y,
