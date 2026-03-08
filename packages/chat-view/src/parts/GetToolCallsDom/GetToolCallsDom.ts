479a7bea3f0b6b76534444d7659378869c8dd584
Le Vivilet <levivilet@pm.me>
2026-03-08 23:16:45 +0100

    feature: simplify getToolCallsDom by removing unused imports and refactoring tool call handling (#194)

--- packages/chat-view/src/parts/GetToolCallsDom/GetToolCallsDom.ts
+++ packages/chat-view/src/parts/GetToolCallsDom/GetToolCallsDom.ts
@@ -1,22 +1,10 @@
-import { type VirtualDomNode, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { type VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { ChatMessage } from '../ChatState/ChatState.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import { getToolCallArgumentPreview } from '../GetToolCallArgumentPreview/GetToolCallArgumentPreview.ts'
+import { getToolCallDom } from '../GetToolCallDom/GetToolCallDom.ts'
 
 export const getToolCallsDom = (message: ChatMessage): readonly VirtualDomNode[] => {
   if (message.role !== 'assistant' || !message.toolCalls || message.toolCalls.length === 0) {
     return []
   }
-  return message.toolCalls.flatMap((toolCall) => {
-    const argumentPreview = getToolCallArgumentPreview(toolCall.arguments)
-    const label = `${toolCall.name} ${argumentPreview}`
-    return [
-      {
-        childCount: 1,
-        className: ClassNames.Markdown,
-        type: VirtualDomElements.P,
-      },
-      text(label),
-    ]
-  })
+  return message.toolCalls.flatMap(getToolCallDom)
 }
