0a4fe826d5ae4c7fae2698164c99c39aeb0d8dfa
Le Vivilet <levivilet@pm.me>
2026-03-08 19:43:14 +0100

    fix

--- packages/chat-view/src/parts/GetAiResponse/GetMockAiResponse/GetMockAiResponse.ts
+++ packages/chat-view/src/parts/GetAiResponse/GetMockAiResponse/GetMockAiResponse.ts
@@ -1,4 +1,4 @@
-import { delay } from './Delay.ts'
+import { delay } from '../Delay/Delay.ts'
 
 export const getMockAiResponse = async (userMessage: string, delayInMs: number): Promise<string> => {
   await delay(delayInMs)
