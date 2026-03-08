4a6cc39560fc69779dff595ed1b687868b7d91a6
Le Vivilet <levivilet@pm.me>
2026-03-08 19:26:31 +0100

    fix

--- packages/chat-view/src/parts/GetToolCallsDom/GetToolCallsDom.ts
+++ packages/chat-view/src/parts/GetToolCallsDom/GetToolCallsDom.ts
@@ -1,10 +1,18 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { ChatMessage } from '../ChatState/ChatState.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getToolCallDom } from '../GetToolCallDom/GetToolCallDom.ts'
 
 export const getToolCallsDom = (message: ChatMessage): readonly VirtualDomNode[] => {
   if (message.role !== 'assistant' || !message.toolCalls || message.toolCalls.length === 0) {
     return []
   }
-  return message.toolCalls.flatMap(getToolCallDom)
+  return [
+    {
+      childCount: message.toolCalls.length,
+      className: ClassNames.Empty,
+      type: VirtualDomElements.Div,
+    },
+    ...message.toolCalls.flatMap(getToolCallDom),
+  ]
 }
