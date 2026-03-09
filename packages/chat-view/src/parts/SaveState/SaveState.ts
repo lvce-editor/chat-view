95654f3319b15ee61715b7e6dc35abd034ec4647
Le Vivilet <levivilet@pm.me>
2026-03-09 15:51:24 +0100

    fix

--- packages/chat-view/src/parts/SaveState/SaveState.ts
+++ packages/chat-view/src/parts/SaveState/SaveState.ts
@@ -2,16 +2,8 @@ import type { ChatState } from '../ChatState/ChatState.ts'
 import type { SavedState } from '../SavedState/SavedState.ts'
 
 export const saveState = (state: ChatState): SavedState => {
-  const {
-    chatListScrollTop,
-    composerValue,
-    messagesScrollTop,
-    nextMessageId,
-    renamingSessionId,
-    selectedModelId,
-    selectedSessionId,
-    viewMode,
-  } = state
+  const { chatListScrollTop, composerValue, messagesScrollTop, nextMessageId, renamingSessionId, selectedModelId, selectedSessionId, viewMode } =
+    state
   return {
     chatListScrollTop,
     composerValue,
