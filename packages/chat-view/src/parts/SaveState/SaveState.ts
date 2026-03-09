8d2925d479dd87fb8427f1794c1a547a740b2295
Le Vivilet <levivilet@pm.me>
2026-03-09 17:29:59 +0100

    feature: remove unused properties from SavedState and related functions for cleaner state management (#224)

--- packages/chat-view/src/parts/SaveState/SaveState.ts
+++ packages/chat-view/src/parts/SaveState/SaveState.ts
@@ -2,32 +2,16 @@ import type { ChatState } from '../ChatState/ChatState.ts'
 import type { SavedState } from '../SavedState/SavedState.ts'
 
 export const saveState = (state: ChatState): SavedState => {
-  const {
-    chatListScrollTop,
-    composerValue,
-    height,
-    messagesScrollTop,
-    nextMessageId,
-    renamingSessionId,
-    selectedModelId,
-    selectedSessionId,
-    viewMode,
-    width,
-    x,
-    y,
-  } = state
+  const { chatListScrollTop, composerValue, messagesScrollTop, nextMessageId, renamingSessionId, selectedModelId, selectedSessionId, viewMode } =
+    state
   return {
     chatListScrollTop,
     composerValue,
-    height,
     messagesScrollTop,
     nextMessageId,
     renamingSessionId,
     selectedModelId,
     selectedSessionId,
     viewMode,
-    width,
-    x,
-    y,
   }
 }
