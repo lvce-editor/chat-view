f2b7268998a232882d38c2e7b674a27517f8e767
Le Vivilet <levivilet@pm.me>
2026-03-10 23:35:20 +0100

    feature: add AI session title generation support and related preferences (#253)

---
+++ packages/chat-view/src/parts/LoadPreferences/LoadAiSessionTitleGenerationEnabled/LoadAiSessionTitleGenerationEnabled.ts
@@ -0,0 +1,10 @@
+import * as Preferences from '../../Preferences/Preferences.ts'
+
+export const loadAiSessionTitleGenerationEnabled = async (): Promise<boolean> => {
+  try {
+    const savedAiSessionTitleGenerationEnabled = await Preferences.get('chatView.aiSessionTitleGenerationEnabled')
+    return typeof savedAiSessionTitleGenerationEnabled === 'boolean' ? savedAiSessionTitleGenerationEnabled : false
+  } catch {
+    return false
+  }
+}
