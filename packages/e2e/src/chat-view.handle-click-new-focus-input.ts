4f5ae791777c16f324e89af1bc9b610181368097
Le Vivilet <levivilet@pm.me>
2026-03-11 00:14:01 +0100

    feature: add end-to-end test for handling new focus input in chat view

---
+++ packages/e2e/src/chat-view.handle-click-new-focus-input.ts
@@ -0,0 +1,18 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'chat-view.handle-click-new-focus-input'
+
+export const skip = 1
+
+export const test: Test = async ({ Chat, expect, Locator }) => {
+  // arrange
+  await Chat.show()
+  await Chat.reset()
+
+  // act
+  await Chat.handleClickNew()
+
+  // assert
+  const composer = Locator('.MultilineInputBox[name="composer"]')
+  await expect(composer).toBeFocused()
+}
