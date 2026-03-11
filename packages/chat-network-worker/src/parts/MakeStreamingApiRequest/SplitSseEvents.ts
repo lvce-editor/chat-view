3e6b1a0da9326049886bd3a14596d166d7ac003b
Le Vivilet <levivilet@pm.me>
2026-03-11 08:05:01 +0100

    refactor: consolidate SSE handling by removing redundant functions and types

--- packages/chat-network-worker/src/parts/MakeStreamingApiRequest/SplitSseEvents.ts
+++
@@ -1,3 +0,0 @@
-export const splitSseEvents = (chunk: string): string[] => {
-  return chunk.split(/\r?\n\r?\n/)
-}
--- packages/chat-network-worker/src/parts/MakeStreamingApiRequest/StreamingApiRequestSuccessResult.ts
+++
@@ -1,6 +0,0 @@
-export interface StreamingApiRequestSuccessResult {
-  readonly body: readonly unknown[]
-  readonly headers: Record<string, string>
-  readonly statusCode: number
-  readonly type: 'success'
-}
