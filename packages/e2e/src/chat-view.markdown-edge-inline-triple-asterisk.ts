5c212048d8e5d3cbffabb6d91610fcb3a5b10e26
Le Vivilet <levivilet@pm.me>
2026-03-12 10:31:37 +0100

    fix

--- packages/e2e/src/chat-view.markdown-edge-inline-triple-asterisk.ts
+++ packages/e2e/src/chat-view.markdown-edge-inline-triple-asterisk.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.markdown-edge.inline-triple-asterisk'
 
+export const skip = 1
+
 export const test: Test = async ({ Chat, Command, expect, FileSystem, Locator, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await Workspace.setPath(tmpDir)
