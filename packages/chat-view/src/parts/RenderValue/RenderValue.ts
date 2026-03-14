afa608d792125924ca7a45c59f5607cd29b8343b
Le Vivilet <levivilet@pm.me>
2026-03-14 09:43:07 +0100

    fix: destructure uid from newState in renderValue function for clarity (#358)

--- packages/chat-view/src/parts/RenderValue/RenderValue.ts
+++ packages/chat-view/src/parts/RenderValue/RenderValue.ts
@@ -3,6 +3,6 @@ import type { ChatState } from '../ChatState/ChatState.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const renderValue = (oldState: ChatState, newState: ChatState): readonly [string, number, string, string] => {
-  const { composerValue } = newState
-  return [ViewletCommand.SetValueByName, newState.uid, InputName.Composer, composerValue]
+  const { composerValue, uid } = newState
+  return [ViewletCommand.SetValueByName, uid, InputName.Composer, composerValue]
 }
