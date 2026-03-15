36c4b907048d94dcbe5e6dccde8393642bc36dd5
Le Vivilet <levivilet@pm.me>
2026-03-15 23:12:05 +0100

    feature: move chat-tool-worker to separate repo

--- packages/chat-tool-worker/src/parts/Listen/Listen.ts
+++
@@ -1,8 +0,0 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import * as CommandMap from '../CommandMap/CommandMap.ts'
-
-export const listen = async (): Promise<void> => {
-  await WebWorkerRpcClient.create({
-    commandMap: CommandMap.commandMap,
-  })
-}
