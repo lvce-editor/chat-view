3d9e1eab3e16cc3e5bb3d51389aaab5edf3bb2c6
Le Vivilet <levivilet@pm.me>
2026-03-15 08:08:27 +0100

    fix

--- packages/chat-view/src/parts/HandleTextChunkFunction/HandleTextChunkFunction.ts
+++ packages/chat-view/src/parts/HandleTextChunkFunction/HandleTextChunkFunction.ts
@@ -1,7 +1,7 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { ChatSession, ChatState } from '../ChatState/ChatState.ts'
-import { copyParsedMessageContent, parseAndStoreMessageContent } from '../ParsedMessageContent/ParsedMessageContent.ts'
 import type { StreamingToolCall } from '../StreamingToolCall/StreamingToolCall.ts'
+import { copyParsedMessageContent, parseAndStoreMessageContent } from '../ParsedMessageContent/ParsedMessageContent.ts'
 import { set } from '../StatusBarStates/StatusBarStates.ts'
 
 export interface HandleTextChunkState {
