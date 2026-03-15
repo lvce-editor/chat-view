35d61c7e86e2a7bb0b4c4844ecd9bf9c2f6808c7
Le Vivilet <levivilet@pm.me>
2026-03-15 23:15:55 +0100

    refactor: remove TODO comment from handleClickSettings function

--- packages/chat-view/src/parts/HandleClickSettings/HandleClickSettings.ts
+++ packages/chat-view/src/parts/HandleClickSettings/HandleClickSettings.ts
@@ -1,6 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const handleClickSettings = async (): Promise<void> => {
-  // TODO
   await RendererWorker.invoke('Main.openUri', 'app://settings.json')
 }
