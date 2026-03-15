8e2e8b8ed6dec9eebef3d8389e5d45e55d0c551e
Le Vivilet <levivilet@pm.me>
2026-03-15 17:29:24 +0100

    fix

--- packages/chat-view/src/parts/GetMenuEntryIds/GetMenuEntryIds.ts
+++ packages/chat-view/src/parts/GetMenuEntryIds/GetMenuEntryIds.ts
@@ -1,5 +1,3 @@
-import { MenuEntryId } from '@lvce-editor/constants'
-
 export const getMenuEntryIds = (): readonly number[] => {
-  return [MenuEntryId.ActivityBar, MenuEntryId.ActivityBarAdditionalViews, MenuEntryId.Settings]
+  return []
 }
