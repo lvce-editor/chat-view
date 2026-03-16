0f403a0af3d98ff527513933b4aa431657d599d8
Le Vivilet <levivilet@pm.me>
2026-03-16 07:46:01 +0100

    fix

--- packages/chat-view/src/parts/InitializeOpenerWorker/InitializeOpenerWorker.ts
+++ packages/chat-view/src/parts/InitializeOpenerWorker/InitializeOpenerWorker.ts
@@ -1,5 +1,5 @@
 import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
-import { ChatMathWorker, RendererWorker } from '@lvce-editor/rpc-registry'
+import { OpenerWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 
 const sendMessagePortToOpenerWorker = async (port: MessagePort): Promise<void> => {
   await RendererWorker.sendMessagePortToOpenerWorker(port, 0)
@@ -10,5 +10,5 @@ export const initializeOpenerWorker = async (): Promise<void> => {
     commandMap: {},
     send: sendMessagePortToOpenerWorker,
   })
-  ChatMathWorker.set(rpc)
+  OpenerWorker.set(rpc)
 }
