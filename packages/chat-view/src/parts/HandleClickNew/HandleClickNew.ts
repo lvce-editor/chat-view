5caf7a0a7f88b1b23b2d3f686257d8c4ff6c3697
Le Vivilet <levivilet@pm.me>
2026-03-16 15:40:58 +0100

    feat: enhance handleClickNew to clear input after session creation (#428)

--- packages/chat-view/src/parts/HandleClickNew/HandleClickNew.ts
+++ packages/chat-view/src/parts/HandleClickNew/HandleClickNew.ts
@@ -1,8 +1,10 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
+import { clearInput } from '../ClearInput/ClearInput.ts'
 import { createSession } from '../CreateSession/CreateSession.ts'
 import { focusInput } from '../FocusInput/FocusInput.ts'
 
 export const handleClickNew = async (state: ChatState): Promise<ChatState> => {
   const newState = await createSession(state)
-  return focusInput(newState)
+  const clearedState = await clearInput(newState)
+  return focusInput(clearedState)
 }
