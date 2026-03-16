4b8fee1274b4a7f0e7503eb4a41c687dad9a2fdc
Le Vivilet <levivilet@pm.me>
2026-03-16 12:25:45 +0100

    feat: update test to assert on ChatListItemLabel instead of ChatHeader

--- packages/e2e/src/chat-view.handle-click-new-empty-session-title.ts
+++ packages/e2e/src/chat-view.handle-click-new-empty-session-title.ts
@@ -2,17 +2,15 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.handle-click-new-empty-session-title'
 
-export const skip = 1
-
 export const test: Test = async ({ Chat, expect, Locator }) => {
   // arrange
   await Chat.show()
   await Chat.reset()
-  const headerTitle = Locator('.ChatHeader .ChatName .Label')
+  const label = Locator('.ChatListItemLabel')
 
   // act
   await Chat.handleClickNew()
 
   // assert
-  await expect(headerTitle).toHaveText('Chat 1')
+  await expect(label).toHaveText('Chat 1')
 }
