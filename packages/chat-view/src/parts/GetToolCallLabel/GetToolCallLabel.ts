4df9f1475256a17516c9d5b6ac909f63700fdabf
Le Vivilet <levivilet@pm.me>
2026-03-19 10:19:53 +0100

    feat: implement virtual DOM rendering for various tool calls and add utility functions

---
+++ packages/chat-view/src/parts/GetToolCallLabel/GetToolCallLabel.ts
@@ -0,0 +1,18 @@
+import type { ChatToolCall } from '../ChatMessage/ChatMessage.ts'
+import { getToolCallArgumentPreview } from '../GetToolCallArgumentPreview/GetToolCallArgumentPreview.ts'
+import { getToolCallDisplayName } from '../GetToolCallDisplayName/GetToolCallDisplayName.ts'
+import { getToolCallStatusLabel } from '../GetToolCallStatusLabel/GetToolCallStatusLabel.ts'
+import { hasIncompleteJsonArguments } from '../HasIncompleteJsonArguments/HasIncompleteJsonArguments.ts'
+
+export const getToolCallLabel = (toolCall: ChatToolCall): string => {
+  const displayName = getToolCallDisplayName(toolCall.name)
+  if (toolCall.name === 'write_file' && !toolCall.status && hasIncompleteJsonArguments(toolCall.arguments)) {
+    return `${displayName} (in progress)`
+  }
+  const argumentPreview = getToolCallArgumentPreview(toolCall.arguments)
+  const statusLabel = getToolCallStatusLabel(toolCall)
+  if (argumentPreview === '{}') {
+    return `${displayName}${statusLabel}`
+  }
+  return `${displayName} ${argumentPreview}${statusLabel}`
+}
---
+++ packages/chat-view/src/parts/GetToolCallWriteFileVirtualDom/GetToolCallWriteFileVirtualDom.ts
@@ -0,0 +1,58 @@
+import { type VirtualDomNode, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
+import type { ChatToolCall } from '../ChatMessage/ChatMessage.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import { getFileNameFromUri } from '../GetFileNameFromUri/GetFileNameFromUri.ts'
+import { getReadFileTarget } from '../GetReadFileTarget/GetReadFileTarget.ts'
+import { getToolCallStatusLabel } from '../GetToolCallStatusLabel/GetToolCallStatusLabel.ts'
+import { parseWriteFileLineCounts } from '../ParseWriteFileLineCounts/ParseWriteFileLineCounts.ts'
+
+export const getToolCallWriteFileVirtualDom = (toolCall: ChatToolCall): readonly VirtualDomNode[] => {
+  const target = getReadFileTarget(toolCall.arguments)
+  if (!target) {
+    return []
+  }
+  const fileName = getFileNameFromUri(target.title)
+  const statusLabel = getToolCallStatusLabel(toolCall)
+  const { linesAdded, linesDeleted } = parseWriteFileLineCounts(toolCall.result)
+  const fileNameClickableProps = target.clickableUri
+    ? {
+        'data-uri': target.clickableUri,
+        onClick: DomEventListenerFunctions.HandleClickReadFile,
+      }
+    : {}
+  return [
+    {
+      childCount: statusLabel ? 6 : 5,
+      className: ClassNames.ChatOrderedListItem,
+      title: target.title,
+      type: VirtualDomElements.Li,
+    },
+    {
+      childCount: 0,
+      className: ClassNames.FileIcon,
+      type: VirtualDomElements.Div,
+    },
+    text('write_file '),
+    {
+      childCount: 1,
+      className: ClassNames.ChatToolCallReadFileLink,
+      ...fileNameClickableProps,
+      type: VirtualDomElements.Span,
+    },
+    text(fileName),
+    {
+      childCount: 1,
+      className: ClassNames.Insertion,
+      type: VirtualDomElements.Span,
+    },
+    text(` +${linesAdded}`),
+    {
+      childCount: 1,
+      className: ClassNames.Deletion,
+      type: VirtualDomElements.Span,
+    },
+    text(` -${linesDeleted}`),
+    ...(statusLabel ? [text(statusLabel)] : []),
+  ]
+}
