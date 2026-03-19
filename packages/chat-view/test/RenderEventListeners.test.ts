0e11fc0cf9084f77bf745ad79311d0d6350de756
Le Vivilet <levivilet@pm.me>
2026-03-19 13:57:34 +0100

    lint

--- packages/chat-view/test/RenderEventListeners.test.ts
+++ packages/chat-view/test/RenderEventListeners.test.ts
@@ -1,6 +1,4 @@
 import { expect, test } from '@jest/globals'
-import { EventExpression } from '@lvce-editor/constants'
-import * as DomEventListenersFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 
 test('renderEventListeners should return expected listeners', () => {
