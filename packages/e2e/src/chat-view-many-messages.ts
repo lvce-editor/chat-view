eb93cbd5dcb4d1500fece3299372f77205b0a405
Le Vivilet <levivilet@pm.me>
2026-03-19 18:27:44 +0100

    feat: update end-to-end test to include workspace setup and remove redundant assertions (#482)

--- packages/e2e/src/chat-view-many-messages.ts
+++ packages/e2e/src/chat-view-many-messages.ts
@@ -2,21 +2,18 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.one-message'
 
-export const skip = 1
-
-export const test: Test = async ({ Chat, expect, Locator }) => {
+export const test: Test = async ({ Chat, expect, FileSystem, Locator, Workspace }) => {
   // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
   await Chat.show()
+  await Chat.reset()
 
   // act
   const messages = Locator('.ChatMessages .Message')
   for (let i = 0; i < 20; i++) {
     await Chat.handleInput('hello from e2e')
     await Chat.handleSubmit()
-    // const firstMessage = messages.nth(i * 2)
-    // await expect(firstMessage).toHaveText('hello from e2e')
-    // const secondMessage = messages.nth(i * 2 - 1)
-    // await expect(secondMessage).toHaveText('Mock AI response: I received "hello from e2e".')
   }
 
   // assert
