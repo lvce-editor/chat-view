08b85ed914e71a4af73e1fed8149556f4a150a5a
Le Vivilet <levivilet@pm.me>
2026-03-20 07:38:48 +0100

    test: add assertions for send area height in chat view new line test

--- packages/e2e/src/chat-view.new-line-3.ts
+++ packages/e2e/src/chat-view.new-line-3.ts
@@ -17,4 +17,6 @@ export const test: Test = async ({ Chat, expect, Locator }) => {
   // assert
   const input = Locator('.Chat .MultilineInputBox')
   await expect(input).toHaveCSS('height', '94px')
+  const sendArea = Locator('.ChatSendArea')
+  await expect(sendArea).toHaveCSS('height', '164px')
 }
