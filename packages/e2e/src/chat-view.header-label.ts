272e9ed3e023c4ffe8ab904a9a2c41de3738d2df
Le Vivilet <levivilet@pm.me>
2026-03-20 12:06:53 +0100

    fix

--- packages/e2e/src/chat-view.header-label.ts
+++ packages/e2e/src/chat-view.header-label.ts
@@ -3,8 +3,10 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 export const name = 'chat-view.header-label'
 
 export const test: Test = async ({ Chat, expect, Locator }) => {
+  // arrange
   await Chat.show()
 
+  // assert
   const label = Locator('.ChatHeader .Label')
   await expect(label).toHaveText('Chats')
   await expect(label).toHaveCSS('text-decoration', 'underline')
