43bbdfa1adfecc962d0bcf350d107170992ab824
Le Vivilet <levivilet@pm.me>
2026-03-20 07:40:57 +0100

    feat: add end-to-end test for multiline input handling in chat view

---
+++ packages/e2e/src/chat-view.new-line-100.ts
@@ -0,0 +1,22 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'chat-view.new-line-1'
+
+export const test: Test = async ({ Chat, expect, Locator }) => {
+  // arrange
+  await Chat.show()
+  await Chat.reset()
+  const composer = Locator('.MultilineInputBox[name="composer"]')
+  await expect(composer).toBeVisible()
+
+  // act
+  for (let i = 0; i < 100; i++) {
+    await Chat.enterNewLine()
+  }
+
+  // assert
+
+  const sendArea = Locator('.ChatSendArea')
+  await expect(sendArea).toHaveCSS('height', '178px')
+  // TODO verify height
+}
