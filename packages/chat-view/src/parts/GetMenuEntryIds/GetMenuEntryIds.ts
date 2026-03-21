152be5970dd5249976db7b9b5d4a1e2abb52e9e6
Le Vivilet <levivilet@pm.me>
2026-03-21 14:13:26 +0100

    feat: implement project management features in context menu and menu entries (#559)

--- packages/chat-view/src/parts/GetMenuEntryIds/GetMenuEntryIds.ts
+++ packages/chat-view/src/parts/GetMenuEntryIds/GetMenuEntryIds.ts
@@ -3,7 +3,8 @@ import { MenuEntryId } from '@lvce-editor/constants'
 export const MenuChatList = 2178
 export const MenuChatHeader = 2179
 export const MenuChatInput = 2180
+export const MenuChatProjectList = 2181
 
 export const getMenuEntryIds = (): readonly number[] => {
-  return [MenuEntryId.Chat, MenuChatList, MenuChatHeader, MenuChatInput]
+  return [MenuEntryId.Chat, MenuChatList, MenuChatHeader, MenuChatInput, MenuChatProjectList]
 }
