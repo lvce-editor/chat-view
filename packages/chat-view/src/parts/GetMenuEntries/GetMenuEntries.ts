f4ff14fb398aec949a1fc0427ea2aa509154b3e5
Le Vivilet <levivilet@pm.me>
2026-03-21 16:43:05 +0100

    refactor: remove unused menu entries and related code

--- packages/chat-view/src/parts/GetMenuEntries/GetMenuEntries.ts
+++ packages/chat-view/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -1,100 +1,9 @@
-import { MenuItemFlags } from '@lvce-editor/constants'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
-import * as ViewletExplorerStrings from '../ExplorerStrings/ExplorerStrings.ts'
 import { getMenuEntriesChatHeader } from '../GetMenuEntriesChatHeader/GetMenuEntriesChatHeader.ts'
 import { getMenuEntriesChatInput } from '../GetMenuEntriesChatInput/GetMenuEntriesChatInput.ts'
 import { getMenuEntriesChatList } from '../GetMenuEntriesChatList/GetMenuEntriesChatList.ts'
 import { getMenuEntriesChatProjectList } from '../GetMenuEntriesChatProjectList/GetMenuEntriesChatProjectList.ts'
 import { MenuChatHeader, MenuChatInput, MenuChatList, MenuChatProjectList } from '../GetMenuEntryIds/GetMenuEntryIds.ts'
-import * as MenuEntrySeparator from '../MenuEntrySeparator/MenuEntrySeparator.ts'
-
-const menuEntryOpenContainingFolder: MenuEntry = {
-  command: 'Explorer.openContainingFolder',
-  flags: MenuItemFlags.RestoreFocus,
-  id: 'openContainingFolder',
-  label: ViewletExplorerStrings.openContainingFolder(),
-}
-
-const menuEntryOpenInIntegratedTerminal: MenuEntry = {
-  command: /* TODO */ '-1',
-  flags: MenuItemFlags.None,
-  id: 'openInIntegratedTerminal',
-  label: ViewletExplorerStrings.openInIntegratedTerminal(),
-}
-
-const menuEntryCut: MenuEntry = {
-  command: 'Explorer.handleCut',
-  flags: MenuItemFlags.RestoreFocus,
-  id: 'cut',
-  label: ViewletExplorerStrings.cut(),
-}
-
-const menuEntryCopy: MenuEntry = {
-  command: 'Explorer.handleCopy',
-  flags: MenuItemFlags.RestoreFocus,
-  id: 'copy',
-  label: ViewletExplorerStrings.copy(),
-}
-
-const menuEntryPaste: MenuEntry = {
-  command: 'Explorer.handlePaste',
-  flags: MenuItemFlags.None,
-  id: 'paste',
-  label: ViewletExplorerStrings.paste(),
-}
-
-const menuEntryCopyPath: MenuEntry = {
-  command: 'Explorer.copyPath',
-  flags: MenuItemFlags.RestoreFocus,
-  id: 'copyPath',
-  label: ViewletExplorerStrings.copyPath(),
-}
-
-const menuEntryCopyAsE2eTest: MenuEntry = {
-  command: 'Chat.copyAsE2eTest',
-  flags: MenuItemFlags.None,
-  id: 'copyAsE2eTest',
-  label: ViewletExplorerStrings.copyPath(),
-}
-
-const menuEntryCopyRelativePath: MenuEntry = {
-  command: 'Explorer.copyRelativePath',
-  flags: MenuItemFlags.RestoreFocus,
-  id: 'copyRelativePath',
-  label: ViewletExplorerStrings.copyRelativePath(),
-}
-
-const menuEntryRename: MenuEntry = {
-  command: 'Explorer.renameDirent',
-  flags: MenuItemFlags.None,
-  id: 'rename',
-  label: ViewletExplorerStrings.rename(),
-}
-
-const menuEntryDelete: MenuEntry = {
-  command: 'Explorer.removeDirent',
-  flags: MenuItemFlags.None,
-  id: 'delete',
-  label: ViewletExplorerStrings.deleteItem(),
-}
-
-const getMenuEntriesFile = (): readonly MenuEntry[] => {
-  return [
-    menuEntryCopyAsE2eTest,
-    menuEntryOpenContainingFolder,
-    menuEntryOpenInIntegratedTerminal,
-    MenuEntrySeparator.menuEntrySeparator,
-    menuEntryCut,
-    menuEntryCopy,
-    menuEntryPaste,
-    MenuEntrySeparator.menuEntrySeparator,
-    menuEntryCopyPath,
-    menuEntryCopyRelativePath,
-    MenuEntrySeparator.menuEntrySeparator,
-    menuEntryRename,
-    menuEntryDelete,
-  ]
-}
 
 export interface ContextMenuProps {
   readonly menuId: number
@@ -112,6 +21,6 @@ export const getMenuEntries = (menuId: number, props: ContextMenuProps): readonl
     case MenuChatProjectList:
       return getMenuEntriesChatProjectList(props.projectId)
     default:
-      return getMenuEntriesFile()
+      return []
   }
 }
