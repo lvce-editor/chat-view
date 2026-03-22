1ed197ba539684a410f850e6ae1cceb10a0134c0
Le Vivilet <levivilet@pm.me>
2026-03-22 07:55:18 +0100

    fix

--- packages/chat-view/src/parts/GetInlineNodeDom/GetInlineNodeDomTypes.ts
+++ packages/chat-view/src/parts/GetInlineNodeDom/GetInlineNodeDomTypes.ts
@@ -1,7 +1,4 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { MessageInlineNode } from '../ParseMessageContentTypes/ParseMessageContentTypes.ts'
 
-export type InlineNodeDomRenderer = (
-  inlineNode: MessageInlineNode,
-  useChatMathWorker: boolean,
-) => readonly VirtualDomNode[]
+export type InlineNodeDomRenderer = (inlineNode: MessageInlineNode, useChatMathWorker: boolean) => readonly VirtualDomNode[]
