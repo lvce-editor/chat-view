e7cbb19698227b77ac2b8e83a469e9ffbe97d8b4
Le Vivilet <levivilet@pm.me>
2026-03-22 08:17:12 +0100

    feat: update resize function to handle asynchronous composer height calculation and modify related tests

--- packages/chat-view/src/parts/Resize/Resize.ts
+++ packages/chat-view/src/parts/Resize/Resize.ts
@@ -1,8 +1,18 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
+import { getComposerHeight } from '../GetComposerHeight/GetComposerHeight.ts'
 
-export const resize = (state: ChatState, dimensions: any): ChatState => {
-  return {
+type GetComposerHeightFn = (state: ChatState, value: string, width: number) => Promise<number>
+
+export const resize = async (state: ChatState, dimensions: any, getComposerHeightFn: GetComposerHeightFn = getComposerHeight): Promise<ChatState> => {
+  const nextState = {
     ...state,
     ...dimensions,
   }
+  if (dimensions.width !== undefined && dimensions.width !== state.width && nextState.composerValue) {
+    return {
+      ...nextState,
+      composerHeight: await getComposerHeightFn(nextState, nextState.composerValue, nextState.width),
+    }
+  }
+  return nextState
 }
