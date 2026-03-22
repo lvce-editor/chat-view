955fcee2939e7418ea97deb21ec17e6a22d3e27d
Le Vivilet <levivilet@pm.me>
2026-03-22 21:51:02 +0100

    fix

--- packages/chat-view/test/PasteInput.test.ts
+++ packages/chat-view/test/PasteInput.test.ts
@@ -1,13 +1,16 @@
 import { expect, test } from '@jest/globals'
-import * as ClipBoardWorker from '../src/parts/ClipBoardWorker/ClipBoardWorker.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as PasteInput from '../src/parts/PasteInput/PasteInput.ts'
+import { ClipBoardWorker } from '@lvce-editor/rpc-registry'
 
 test('pasteInput should paste clipboard text into composer', async () => {
-  ClipBoardWorker.reset()
+  using mockRpc = ClipBoardWorker.registerMockRpc({
+    'ClipBoard.writeText': async (text: string) => {},
+  })
   await ClipBoardWorker.writeText('hello paste')
   const state = createDefaultState()
   const result = await PasteInput.pasteInput(state)
   expect(result.composerValue).toBe('hello paste')
   expect(result.inputSource).toBe('script')
+  expect(mockRpc.invocations).toEqual([['ClipBoard.writeText', 'hello copy']])
 })
