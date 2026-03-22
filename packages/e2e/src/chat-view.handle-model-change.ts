dd7113f5f85bd813c48af86a6124e1c572eebb9c
Le Vivilet <levivilet@pm.me>
2026-03-22 20:54:26 +0100

    e2e

--- packages/e2e/src/chat-view.handle-model-change.ts
+++ packages/e2e/src/chat-view.handle-model-change.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.handle-model-change'
 
+export const skip = 1
+
 export const test: Test = async ({ Chat, expect, Locator }) => {
   // arrange
   await Chat.show()
