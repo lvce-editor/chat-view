34406bedbc823a06415a0e434db6ecc8642b331f
Le Vivilet <levivilet@pm.me>
2026-03-22 16:41:39 +0100

    feature: add context menu handler to ChatModelPicker popover

--- packages/chat-view/src/parts/GetChatModelPickerVirtualDom/GetChatModelPickerVirtualDom.ts
+++ packages/chat-view/src/parts/GetChatModelPickerVirtualDom/GetChatModelPickerVirtualDom.ts
@@ -47,6 +47,7 @@ export const getChatModelPickerPopOverVirtualDom = (
     {
       childCount: 2,
       className: ClassNames.ChatModelPickerContainer,
+      onContextMenu: DomEventListenerFunctions.HandleContextMenuChatSendAreaBottom,
       style: 'position:absolute;',
       type: VirtualDomElements.Div,
     },
