f5fc100a87ef8623399b87a5d8f837c4fe2798af
Le Vivilet <levivilet@pm.me>
2026-03-22 08:05:05 +0100

    feat: update handleChatInputContextMenu to accept event coordinates and modify related tests (#565)

--- packages/e2e/src/chat-view.chat-input-context-menu.ts
+++ packages/e2e/src/chat-view.chat-input-context-menu.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.chat-input-context-menu-entries'
 
-export const skip = 1
-
 export const test: Test = async ({ Chat, Command, expect, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
@@ -16,8 +14,13 @@ export const test: Test = async ({ Chat, Command, expect, FileSystem, Locator, W
   await Command.execute('Chat.handleChatInputContextMenu', 0, 0)
 
   // assert
-  const renameMenuItem = Locator('.MenuItem:has-text("Rename")')
-  const archiveMenuItem = Locator('.MenuItem:has-text("Archive")')
+  const renameMenuItem = Locator('.MenuItem').nth(0)
   await expect(renameMenuItem).toBeVisible()
+  await expect(renameMenuItem).toHaveText('Cut')
+  const archiveMenuItem = Locator('.MenuItem').nth(1)
   await expect(archiveMenuItem).toBeVisible()
+  await expect(archiveMenuItem).toHaveText('Copy')
+  const item3 = Locator('.MenuItem').nth(2)
+  await expect(item3).toBeVisible()
+  await expect(item3).toHaveText('Paste')
 }
