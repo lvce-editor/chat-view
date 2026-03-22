88e5062ce2189a81ea98783ac6eb92d745aa6e77
Le Vivilet <levivilet@pm.me>
2026-03-22 21:42:30 +0100

    worker

--- packages/chat-view/test/CopyInput.test.ts
+++ packages/chat-view/test/CopyInput.test.ts
@@ -1,13 +1,17 @@
 import { expect, test } from '@jest/globals'
 import * as CopyInput from '../src/parts/CopyInput/CopyInput.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { ClipBoardWorker } from '@lvce-editor/rpc-registry'
 
 test('copyInput should copy composer value to clipboard and keep state', async () => {
+  using mockRpc = ClipBoardWorker.registerMockRpc({
+    'ClipBoard.writeText'() {},
+  })
   const state = {
     ...createDefaultState(),
     composerValue: 'hello copy',
   }
   const result = await CopyInput.copyInput(state)
-  expect(text).toBe('hello copy')
+  expect(mockRpc.invocations).toEqual([['ClipBoard.writeText', 'hello copy']])
   expect(result).toBe(state)
 })
