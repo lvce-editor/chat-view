d20cf3d2f3d4861963bc7cd14194459f51f96ccb
Le Vivilet <levivilet@pm.me>
2026-03-23 21:30:57 +0100

    click

--- packages/chat-view/src/parts/HandleClickModelPickerList/HandleClickModelPickerList.ts
+++ packages/chat-view/src/parts/HandleClickModelPickerList/HandleClickModelPickerList.ts
@@ -1,21 +1,11 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
 import { getModelPickerClickIndex } from '../GetModelPickerClickIndex/GetModelPickerClickIndex.ts'
-import { getModelPickerHeight } from '../GetModelPickerHeight/GetModelPickerHeight.ts'
+import { handleClickModelPickerListIndex } from '../HandleClickModelPickerListIndex/HandleClickModelPickerListIndex.ts'
 
 export const handleClickModelPickerList = async (state: ChatState, eventY = 0): Promise<ChatState> => {
-  const { height, models, visibleModels, y } = state
+  const { height, y } = state
   const itemHeight = 28
   const offset = 90
   const index = getModelPickerClickIndex(y, height, eventY, offset, itemHeight)
-  if (index < 0 || index >= visibleModels.length) {
-    return state
-  }
-  return {
-    ...state,
-    modelPickerHeight: getModelPickerHeight(models.length),
-    modelPickerOpen: false,
-    modelPickerSearchValue: '',
-    selectedModelId: visibleModels[index].id,
-    visibleModels: models,
-  }
+  return handleClickModelPickerListIndex(state, index)
 }
---
+++ packages/chat-view/src/parts/HandleClickModelPickerListIndex/HandleClickModelPickerListIndex.ts
@@ -0,0 +1,17 @@
+import type { ChatState } from '../ChatState/ChatState.ts'
+import { getModelPickerHeight } from '../GetModelPickerHeight/GetModelPickerHeight.ts'
+
+export const handleClickModelPickerListIndex = async (state: ChatState, index: number): Promise<ChatState> => {
+  const { models, visibleModels, y } = state
+  if (index < 0 || index >= visibleModels.length) {
+    return state
+  }
+  return {
+    ...state,
+    modelPickerHeight: getModelPickerHeight(models.length),
+    modelPickerOpen: false,
+    modelPickerSearchValue: '',
+    selectedModelId: visibleModels[index].id,
+    visibleModels: models,
+  }
+}
