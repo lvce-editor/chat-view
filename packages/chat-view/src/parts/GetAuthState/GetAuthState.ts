3f07d11bba0efc3fb942aa805cad2aa12db4ddce
Le Vivilet <levivilet@pm.me>
2026-03-23 12:24:32 +0100

    fix

--- packages/chat-view/src/parts/GetAuthState/GetAuthState.ts
+++ packages/chat-view/src/parts/GetAuthState/GetAuthState.ts
@@ -1,17 +1,8 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
 
 export const getAuthState = (state: ChatState): unknown => {
-  const {
-    authAccessToken,
-    authEnabled,
-    authErrorMessage,
-    authRefreshToken,
-    authStatus,
-    backendUrl,
-    userName,
-    userSubscriptionPlan,
-    userUsedTokens,
-  } = state
+  const { authAccessToken, authEnabled, authErrorMessage, authRefreshToken, authStatus, backendUrl, userName, userSubscriptionPlan, userUsedTokens } =
+    state
 
   return {
     authAccessToken,
