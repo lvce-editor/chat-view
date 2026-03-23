12874b2bd3b868b2c2edc59458062b56edeb5ba5
Le Vivilet <levivilet@pm.me>
2026-03-23 13:38:58 +0100

    refactor: extract getModelPickerHeaderDom function for improved structure in chat model picker popover

--- packages/chat-view/src/parts/GetChatModelPickerPopOverVirtualDom/GetChatModelPickerPopOverVirtualDom.ts
+++ packages/chat-view/src/parts/GetChatModelPickerPopOverVirtualDom/GetChatModelPickerPopOverVirtualDom.ts
@@ -48,24 +48,8 @@ const getChatModelListVirtualDom = (visibleModels: readonly ChatModel[], selecte
   ]
 }
 
-export const getChatModelPickerPopOverVirtualDom = (
-  models: readonly ChatModel[],
-  selectedModelId: string,
-  modelPickerSearchValue: string,
-): readonly VirtualDomNode[] => {
-  const visibleModels = getVisibleModels(models, modelPickerSearchValue)
+const getModelPickerHeaderDom = (modelPickerSearchValue: string): readonly VirtualDomNode[] => {
   return [
-    {
-      childCount: 2,
-      className: ClassNames.ChatModelPickerContainer,
-      onContextMenu: DomEventListenerFunctions.HandleContextMenuChatSendAreaBottom,
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 3 + visibleModels.length,
-      className: ClassNames.ChatModelPicker,
-      type: VirtualDomElements.Div,
-    },
     {
       childCount: 2,
       className: ClassNames.ChatModelPickerHeader,
@@ -94,6 +78,28 @@ export const getChatModelPickerPopOverVirtualDom = (
       className: 'MaskIcon MaskIconSettingsGear',
       type: VirtualDomElements.Div,
     },
+  ]
+}
+
+export const getChatModelPickerPopOverVirtualDom = (
+  models: readonly ChatModel[],
+  selectedModelId: string,
+  modelPickerSearchValue: string,
+): readonly VirtualDomNode[] => {
+  const visibleModels = getVisibleModels(models, modelPickerSearchValue)
+  return [
+    {
+      childCount: 2,
+      className: ClassNames.ChatModelPickerContainer,
+      onContextMenu: DomEventListenerFunctions.HandleContextMenuChatSendAreaBottom,
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 3 + visibleModels.length,
+      className: ClassNames.ChatModelPicker,
+      type: VirtualDomElements.Div,
+    },
+    ...getModelPickerHeaderDom(modelPickerSearchValue),
     ...getChatModelListVirtualDom(visibleModels, selectedModelId),
   ]
 }
