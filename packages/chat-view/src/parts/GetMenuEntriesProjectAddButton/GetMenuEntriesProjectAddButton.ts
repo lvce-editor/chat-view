d0034a824efcecb98dc98521b0fc6f27d08efc97
Le Vivilet <levivilet@pm.me>
2026-03-23 10:16:35 +0100

    refactor: rename ViewletExplorerStrings to ViewletChatStrings in menu entry files (#654)

--- packages/chat-view/src/parts/GetMenuEntriesProjectAddButton/GetMenuEntriesProjectAddButton.ts
+++ packages/chat-view/src/parts/GetMenuEntriesProjectAddButton/GetMenuEntriesProjectAddButton.ts
@@ -1,6 +1,6 @@
 import { MenuItemFlags } from '@lvce-editor/constants'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
-import * as ExplorerStrings from '../ChatStrings/ChatStrings.ts'
+import * as ChatStrings from '../ChatStrings/ChatStrings.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const getMenuEntriesProjectAddButton = (): readonly MenuEntry[] => {
@@ -10,7 +10,7 @@ export const getMenuEntriesProjectAddButton = (): readonly MenuEntry[] => {
       command: 'Chat.handleClick',
       flags: MenuItemFlags.None,
       id: 'addProject',
-      label: ExplorerStrings.addProject(),
+      label: ChatStrings.addProject(),
     },
   ]
 }
