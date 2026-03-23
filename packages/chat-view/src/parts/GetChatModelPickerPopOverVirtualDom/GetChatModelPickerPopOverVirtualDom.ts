becaf25a63f0249aa7e58e76404e7db1a269f7c2
Le Vivilet <levivilet@pm.me>
2026-03-23 13:38:33 +0100

    feat: move getChatModelListVirtualDom function to a new file for better organization

--- packages/chat-view/src/parts/GetChatModelPickerPopOverVirtualDom/GetChatModelPickerPopOverVirtualDom.ts
+++ packages/chat-view/src/parts/GetChatModelPickerPopOverVirtualDom/GetChatModelPickerPopOverVirtualDom.ts
@@ -1,52 +1,11 @@
-import { type VirtualDomNode, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { ChatModel } from '../ChatModel/ChatModel.ts'
 import * as Strings from '../ChatStrings/ChatStrings.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import { getModelLabel } from '../GetModelLabel/GetModelLabel.ts'
-import { getUsageCostLabel } from '../GetUsageCostLabel/GetUsageCostLabel.ts'
+import { getChatModelListVirtualDom } from '../GetChatModelListVirtualDom/GetChatModelListVirtualDom.ts'
 import { getVisibleModels } from '../GetVisibleModels/GetVisibleModels.ts'
 import * as InputName from '../InputName/InputName.ts'
-
-const getChatModelListVirtualDom = (visibleModels: readonly ChatModel[], selectedModelId: string): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: Math.max(visibleModels.length, 1),
-      className: ClassNames.ChatModelPickerList,
-      name: InputName.ModelPickerList,
-      onClick: DomEventListenerFunctions.HandleClick,
-      type: VirtualDomElements.Ul,
-    },
-    ...(visibleModels.length === 0
-      ? [
-          {
-            childCount: 1,
-            className: ClassNames.ChatModelPickerItem,
-            type: VirtualDomElements.Li,
-          },
-          text(Strings.noMatchingModelsFound()),
-        ]
-      : visibleModels.flatMap((model) => [
-          {
-            childCount: 2,
-            className: `${ClassNames.ChatModelPickerItem}${model.id === selectedModelId ? ` ${ClassNames.ChatModelPickerItemSelected}` : ''}`,
-            type: VirtualDomElements.Li,
-          },
-          {
-            childCount: 1,
-            className: ClassNames.ChatModelPickerItemLabel,
-            type: VirtualDomElements.Span,
-          },
-          text(getModelLabel(model)),
-          {
-            childCount: 1,
-            className: ClassNames.ChatModelPickerItemUsageCost,
-            type: VirtualDomElements.Span,
-          },
-          text(getUsageCostLabel(model)),
-        ])),
-  ]
-}
 
 export const getChatModelPickerPopOverVirtualDom = (
   models: readonly ChatModel[],
