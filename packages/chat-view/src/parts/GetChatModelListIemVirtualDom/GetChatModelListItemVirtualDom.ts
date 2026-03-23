f4e61761655bc8ac877491fe979e843a054e13f6
Le Vivilet <levivilet@pm.me>
2026-03-23 15:23:48 +0100

    feat: implement getChatModelListItemVirtualDom function for rendering chat model items

---
+++ packages/chat-view/src/parts/GetChatModelListIemVirtualDom/GetChatModelListItemVirtualDom.ts
@@ -0,0 +1,27 @@
+import { text, VirtualDomElements, VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { ChatModel } from '../ChatModel/ChatModel.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { getModelLabel } from '../GetModelLabel/GetModelLabel.ts'
+import { getUsageCostLabel } from '../GetUsageCostLabel/GetUsageCostLabel.ts'
+
+export const getChatModelListItemVirtualDom = (model: ChatModel, selectedModelId: string): readonly VirtualDomNode[] => {
+  return [
+    {
+      childCount: 2,
+      className: `${ClassNames.ChatModelPickerItem}${model.id === selectedModelId ? ` ${ClassNames.ChatModelPickerItemSelected}` : ''}`,
+      type: VirtualDomElements.Li,
+    },
+    {
+      childCount: 1,
+      className: ClassNames.ChatModelPickerItemLabel,
+      type: VirtualDomElements.Span,
+    },
+    text(getModelLabel(model)),
+    {
+      childCount: 1,
+      className: ClassNames.ChatModelPickerItemUsageCost,
+      type: VirtualDomElements.Span,
+    },
+    text(getUsageCostLabel(model)),
+  ]
+}
--- packages/chat-view/src/parts/GetChatModelListVirtualDom/GetChatModelListVirtualDom.ts
+++ packages/chat-view/src/parts/GetChatModelListVirtualDom/GetChatModelListVirtualDom.ts
@@ -3,46 +3,31 @@ import type { ChatModel } from '../ChatModel/ChatModel.ts'
 import * as Strings from '../ChatStrings/ChatStrings.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import { getModelLabel } from '../GetModelLabel/GetModelLabel.ts'
-import { getUsageCostLabel } from '../GetUsageCostLabel/GetUsageCostLabel.ts'
-import * as InputName from '../InputName/InputName.ts'
+import { getChatModelListItemVirtualDom } from '../GetChatModelListIemVirtualDom/GetChatModelListItemVirtualDom.ts'
 
-export const getChatModelListVirtualDom = (visibleModels: readonly ChatModel[], selectedModelId: string): readonly VirtualDomNode[] => {
+const getNoMatchingModelsFoundVirtualDom = (): readonly VirtualDomNode[] => {
   return [
     {
-      childCount: Math.max(visibleModels.length, 1),
+      childCount: 1,
+      className: 'Message',
+      type: VirtualDomElements.P,
+    },
+    text(Strings.noMatchingModelsFound()),
+  ]
+}
+
+export const getChatModelListVirtualDom = (visibleModels: readonly ChatModel[], selectedModelId: string): readonly VirtualDomNode[] => {
+  if (visibleModels.length === 0) {
+    return getNoMatchingModelsFoundVirtualDom()
+  }
+  return [
+    {
+      childCount: visibleModels.length,
       className: ClassNames.ChatModelPickerList,
-      name: InputName.ModelPickerList,
       onClick: DomEventListenerFunctions.HandleClick,
       type: VirtualDomElements.Ul,
     },
-    ...(visibleModels.length === 0
-      ? [
-          {
-            childCount: 1,
-            className: ClassNames.ChatModelPickerItem,
-            type: VirtualDomElements.Li,
-          },
-          text(Strings.noMatchingModelsFound()),
-        ]
-      : visibleModels.flatMap((model) => [
-          {
-            childCount: 2,
-            className: `${ClassNames.ChatModelPickerItem}${model.id === selectedModelId ? ` ${ClassNames.ChatModelPickerItemSelected}` : ''}`,
-            type: VirtualDomElements.Li,
-          },
-          {
-            childCount: 1,
-            className: ClassNames.ChatModelPickerItemLabel,
-            type: VirtualDomElements.Span,
-          },
-          text(getModelLabel(model)),
-          {
-            childCount: 1,
-            className: ClassNames.ChatModelPickerItemUsageCost,
-            type: VirtualDomElements.Span,
-          },
-          text(getUsageCostLabel(model)),
-        ])),
+
+    ...visibleModels.flatMap((model) => getChatModelListItemVirtualDom(model, selectedModelId)),
   ]
 }
