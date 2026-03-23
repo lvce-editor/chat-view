de35eb99eec18ec0fbe6ae0ee00e14a0b9c70005
Le Vivilet <levivilet@pm.me>
2026-03-23 13:26:58 +0100

    feat: implement getChatModelPickerToggleVirtualDom for model picker toggle functionality

---
+++ packages/chat-view/src/parts/GetChatModelPickerToggleVirtualDom/GetChatModelPickerToggleVirtualDom.ts
@@ -0,0 +1,36 @@
+import { type VirtualDomNode, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { ChatModel } from '../ChatModel/ChatModel.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as InputName from '../InputName/InputName.ts'
+
+export const getChatModelPickerToggleVirtualDom = (
+  models: readonly ChatModel[],
+  selectedModelId: string,
+  modelPickerOpen: boolean,
+): readonly VirtualDomNode[] => {
+  const selectedModel = models.find((model) => model.id === selectedModelId)
+  const selectedModelLabel = selectedModel ? selectedModel.name : selectedModelId
+  return [
+    {
+      childCount: 2,
+      className: ClassNames.Select,
+      name: InputName.ModelPickerToggle,
+      onClick: DomEventListenerFunctions.HandleClickModelPickerToggle,
+      title: selectedModelLabel,
+      type: VirtualDomElements.Button,
+    },
+    {
+      childCount: 1,
+      className: ClassNames.SelectLabel,
+      type: VirtualDomElements.Span,
+    },
+    text(selectedModelLabel),
+    {
+      childCount: 0,
+      className: modelPickerOpen ? 'MaskIcon MaskIconChevronUp' : 'MaskIcon MaskIconChevronDown',
+      role: 'none',
+      type: VirtualDomElements.Div,
+    },
+  ]
+}
--- packages/chat-view/src/parts/GetChatModelPickerVirtualDom/GetChatModelPickerVirtualDom.ts
+++ packages/chat-view/src/parts/GetChatModelPickerVirtualDom/GetChatModelPickerVirtualDom.ts
@@ -1,40 +1,7 @@
-import { type VirtualDomNode, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { ChatModel } from '../ChatModel/ChatModel.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getChatModelPickerPopOverVirtualDom } from '../GetChatModelPickerPopOverVirtualDom/GetChatModelPickerPopOverVirtualDom.ts'
-import * as InputName from '../InputName/InputName.ts'
-
-export const getChatModelPickerToggleVirtualDom = (
-  models: readonly ChatModel[],
-  selectedModelId: string,
-  modelPickerOpen: boolean,
-): readonly VirtualDomNode[] => {
-  const selectedModel = models.find((model) => model.id === selectedModelId)
-  const selectedModelLabel = selectedModel ? selectedModel.name : selectedModelId
-  return [
-    {
-      childCount: 2,
-      className: ClassNames.Select,
-      name: InputName.ModelPickerToggle,
-      onClick: DomEventListenerFunctions.HandleClickModelPickerToggle,
-      title: selectedModelLabel,
-      type: VirtualDomElements.Button,
-    },
-    {
-      childCount: 1,
-      className: ClassNames.SelectLabel,
-      type: VirtualDomElements.Span,
-    },
-    text(selectedModelLabel),
-    {
-      childCount: 0,
-      className: modelPickerOpen ? 'MaskIcon MaskIconChevronUp' : 'MaskIcon MaskIconChevronDown',
-      role: 'none',
-      type: VirtualDomElements.Div,
-    },
-  ]
-}
+import { getChatModelPickerToggleVirtualDom } from '../GetChatModelPickerToggleVirtualDom/GetChatModelPickerToggleVirtualDom.ts'
 
 export const getChatModelPickerVirtualDom = (
   models: readonly ChatModel[],
