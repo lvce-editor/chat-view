0a58efedd6fbaaa459285044e3347ab0c337331e
Le Vivilet <levivilet@pm.me>
2026-03-23 22:02:41 +0100

    lint

--- packages/chat-view/src/parts/SetStreamingEnabled/SetStreamingEnabled.ts
+++ packages/chat-view/src/parts/SetStreamingEnabled/SetStreamingEnabled.ts
@@ -1,5 +1,6 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
 
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 export const setStreamingEnabled = (state: ChatState, streamingEnabled: boolean): ChatState => {
   return {
     ...state,
--- packages/chat-view/src/parts/UseMockApi/UseMockApi.ts
+++ packages/chat-view/src/parts/UseMockApi/UseMockApi.ts
@@ -2,6 +2,7 @@ import type { ChatState } from '../ChatState/ChatState.ts'
 
 const defaultMockApiCommandId = 'ChatE2e.mockApi'
 
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 export const useMockApi = (state: ChatState, value: boolean, mockApiCommandId: string = defaultMockApiCommandId): ChatState => {
   if (!value) {
     return {
