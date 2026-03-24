6bd68522d67f12981647cfa6847b94d6be46d0d0
Le Vivilet <levivilet@pm.me>
2026-03-24 07:11:05 +0100

    refactor: simplify getEmptyChatSessionsDom by extracting node definitions

--- packages/chat-view/src/parts/GetEmptyChatSessionsDom/GetEmptyChatSessionsDom.ts
+++ packages/chat-view/src/parts/GetEmptyChatSessionsDom/GetEmptyChatSessionsDom.ts
@@ -2,14 +2,18 @@ import { type VirtualDomNode, text, VirtualDomElements } from '@lvce-editor/virt
 import * as Strings from '../ChatStrings/ChatStrings.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 
+const parentNode: VirtualDomNode = {
+  childCount: 1,
+  className: ClassNames.ChatListEmpty,
+  type: VirtualDomElements.Div,
+}
+
+const labelNode: VirtualDomNode = {
+  childCount: 1,
+  className: ClassNames.Label,
+  type: VirtualDomElements.Div,
+}
+
 export const getEmptyChatSessionsDom = (): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.ChatListEmpty,
-      type: VirtualDomElements.Div,
-    },
-    { childCount: 1, className: ClassNames.Label, type: VirtualDomElements.Div },
-    text(Strings.clickToOpenNewChat()),
-  ]
+  return [parentNode, labelNode, text(Strings.clickToOpenNewChat())]
 }
