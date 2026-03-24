a9bd0d1abeb3e1dc56378bdd2bd7a0136d120925
Le Vivilet <levivilet@pm.me>
2026-03-24 07:31:38 +0100

    refactor: move ContextMenuProps interface to its own file for better organization

--- packages/chat-view/src/parts/GetMenuEntries/GetMenuEntries.ts
+++ packages/chat-view/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -1,15 +1,11 @@
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
+import type { ContextMenuProps } from './ContextMenuProps/ContextMenuProps.ts'
 import { getMenuEntriesChatHeader } from '../GetMenuEntriesChatHeader/GetMenuEntriesChatHeader.ts'
 import { getMenuEntriesChatInput } from '../GetMenuEntriesChatInput/GetMenuEntriesChatInput.ts'
 import { getMenuEntriesChatList } from '../GetMenuEntriesChatList/GetMenuEntriesChatList.ts'
 import { getMenuEntriesChatProjectList } from '../GetMenuEntriesChatProjectList/GetMenuEntriesChatProjectList.ts'
 import { getMenuEntriesProjectAddButton } from '../GetMenuEntriesProjectAddButton/GetMenuEntriesProjectAddButton.ts'
 import { MenuChatHeader, MenuChatInput, MenuChatList, MenuChatProjectList, MenuProjectAddButton } from '../GetMenuEntryIds/GetMenuEntryIds.ts'
-
-export interface ContextMenuProps {
-  readonly menuId: number
-  readonly [key: string]: any
-}
 
 export const getMenuEntries = (menuId: number, props: ContextMenuProps): readonly MenuEntry[] => {
   switch (props.menuId) {
