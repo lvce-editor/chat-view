6530d3297113dfbf1f7b350739745ec0bc188867
Le Vivilet <levivilet@pm.me>
2026-03-24 07:16:17 +0100

    fix: add missing newline at the end of GetMessageById.ts

--- packages/chat-view/src/parts/GetMessageById/GetMessageById.ts
+++ packages/chat-view/src/parts/GetMessageById/GetMessageById.ts
@@ -1,4 +1,5 @@
 import type { ChatMessage } from '../ChatMessage/ChatMessage.ts'
+
 export const getMessageById = (messages: readonly ChatMessage[], messageId: string): ChatMessage | undefined => {
   return messages.find((message) => message.id === messageId)
 }
