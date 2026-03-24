49b9802f660290d7e8376040c47a6a517e8d412f
Le Vivilet <levivilet@pm.me>
2026-03-24 07:12:39 +0100

    refactor: change InlineNodeDomRenderer type to interface for improved structure

--- packages/chat-view/src/parts/GetInlineNodeDom/GetInlineNodeDomTypes.ts
+++ packages/chat-view/src/parts/GetInlineNodeDom/GetInlineNodeDomTypes.ts
@@ -2,4 +2,6 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { MessageInlineNode } from '../ParseMessageContentTypes/ParseMessageContentTypes.ts'
 
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-export type InlineNodeDomRenderer = (inlineNode: MessageInlineNode, useChatMathWorker: boolean) => readonly VirtualDomNode[]
+export interface InlineNodeDomRenderer {
+  (inlineNode: MessageInlineNode, useChatMathWorker: boolean): readonly VirtualDomNode[]
+}
