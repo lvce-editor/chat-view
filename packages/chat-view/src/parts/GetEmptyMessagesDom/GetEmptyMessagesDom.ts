476bcbe619febccd3cd8c117318ef929753298e6
Le Vivilet <levivilet@pm.me>
2026-03-24 07:15:05 +0100

    refactor: extract parentNode definition for clarity in getEmptyMessagesDom function (#715)

--- packages/chat-view/src/parts/GetEmptyMessagesDom/GetEmptyMessagesDom.ts
+++ packages/chat-view/src/parts/GetEmptyMessagesDom/GetEmptyMessagesDom.ts
@@ -3,14 +3,13 @@ import * as Strings from '../ChatStrings/ChatStrings.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
+const parentNode: VirtualDomNode = {
+  childCount: 1,
+  className: ClassNames.ChatWelcomeMessage,
+  onContextMenu: DomEventListenerFunctions.HandleChatWelcomeContextMenu,
+  type: VirtualDomElements.Div,
+}
+
 export const getEmptyMessagesDom = (): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.ChatWelcomeMessage,
-      onContextMenu: DomEventListenerFunctions.HandleChatWelcomeContextMenu,
-      type: VirtualDomElements.Div,
-    },
-    text(Strings.startConversation()),
-  ]
+  return [parentNode, text(Strings.startConversation())]
 }
