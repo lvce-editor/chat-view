c089706182967c3e924c8da82d4705d789c41358
Le Vivilet <levivilet@pm.me>
2026-03-24 10:09:14 +0100

    feat: implement create_directory tool call rendering and associated tests (#748)

---
+++ packages/chat-view/src/parts/GetToolCallCreateDirectoryVirtualDom/GetToolCallCreateDirectoryVirtualDom.ts
@@ -0,0 +1,55 @@
+import { type VirtualDomNode, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
+import type { ChatToolCall } from '../ChatMessage/ChatMessage.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import { getFileNameFromUri } from '../GetFileNameFromUri/GetFileNameFromUri.ts'
+import { getReadFileTarget } from '../GetReadFileTarget/GetReadFileTarget.ts'
+import { getToolCallStatusLabel } from '../GetToolCallStatusLabel/GetToolCallStatusLabel.ts'
+
+export const getToolCallCreateDirectoryVirtualDom = (toolCall: ChatToolCall): readonly VirtualDomNode[] => {
+  const target = getReadFileTarget(toolCall.arguments)
+  if (!target) {
+    return []
+  }
+  const directoryName = getFileNameFromUri(target.title)
+  const statusLabel = getToolCallStatusLabel(toolCall)
+  const fileNameClickableProps = target.clickableUri
+    ? {
+        'data-uri': target.clickableUri,
+        onClick: DomEventListenerFunctions.HandleClickFileName,
+      }
+    : {}
+  return [
+    {
+      childCount: statusLabel ? 4 : 3,
+      className: ClassNames.ChatOrderedListItem,
+      title: target.title,
+      type: VirtualDomElements.Li,
+    },
+    {
+      childCount: 0,
+      className: ClassNames.FileIcon,
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 1,
+      className: ClassNames.ToolCallName,
+      type: VirtualDomElements.Span,
+    },
+    text('create_directory '),
+    {
+      childCount: 1,
+      className: ClassNames.ChatToolCallReadFileLink,
+      ...fileNameClickableProps,
+      type: VirtualDomElements.Span,
+    },
+    {
+      childCount: 1,
+      className: ClassNames.ChatToolCallFileName,
+      ...fileNameClickableProps,
+      type: VirtualDomElements.Span,
+    },
+    text(directoryName),
+    ...(statusLabel ? [text(statusLabel)] : []),
+  ]
+}
