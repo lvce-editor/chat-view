7b1026f9caae10839b947fcb62066365ca4453e8
Le Vivilet <levivilet@pm.me>
2026-03-25 11:49:28 +0100

    fix

--- packages/chat-view/src/parts/SetUseChatMessageParsingWorker/SetUseChatMessageParsingWorker.ts
+++ packages/chat-view/src/parts/SetUseChatMessageParsingWorker/SetUseChatMessageParsingWorker.ts
@@ -1,7 +1,6 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
 import * as Preferences from '../Preferences/Preferences.ts'
 
- 
 export const setUseChatMessageParsingWorker = async (state: ChatState, useChatMessageParsingWorker: boolean, persist = true): Promise<ChatState> => {
   if (persist) {
     await Preferences.update({
