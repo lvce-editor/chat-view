fe00f9dd17f517a6e398aefd30f3cb368309bf0b
Le Vivilet <levivilet@pm.me>
2026-03-25 11:36:32 +0100

    feat: add new test for chat input height adjustment on character input (#774)

---
+++ packages/e2e/src/chat-view.one-character-typed.ts
@@ -0,0 +1,20 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'chat-view.new-line-1'
+
+export const test: Test = async ({ Chat, expect, Locator }) => {
+  // arrange
+  await Chat.show()
+  await Chat.reset()
+  const composer = Locator('.ChatInputBox[name="composer"]')
+  await expect(composer).toBeVisible()
+
+  // act
+  await Chat.handleInput('a')
+
+  // assert
+  const input = Locator('.Chat .ChatInputBox')
+  await expect(input).toHaveCSS('height', '34px') // TODO
+  const sendArea = Locator('.ChatSendArea')
+  await expect(sendArea).toHaveCSS('height', '104px')
+}
