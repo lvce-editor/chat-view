eee5286e2a3916c807c70245e35179e1dfffacec
Le Vivilet <levivilet@pm.me>
2026-03-25 07:44:05 +0100

    feature: add blur listener (#758)

---
+++ packages/chat-view/src/parts/HandleModelInputBlur/HandleModelInputBlur.ts
@@ -0,0 +1,19 @@
+import type { ChatState } from '../ChatState/ChatState.ts'
+import { getModelPickerHeight } from '../GetModelPickerHeight/GetModelPickerHeight.ts'
+
+export const handleModelInputBlur = async (state: ChatState): Promise<ChatState> => {
+  if (!state.modelPickerOpen) {
+    return state
+  }
+  return {
+    ...state,
+    focus: 'composer',
+    focused: true,
+    listFocusedIndex: -1,
+    modelPickerHeight: getModelPickerHeight(state.models.length),
+    modelPickerListScrollTop: 0,
+    modelPickerOpen: false,
+    modelPickerSearchValue: '',
+    visibleModels: state.models,
+  }
+}
