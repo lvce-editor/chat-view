7cdb79ee56151bce4066b31d2e00bcb28cd050f9
Le Vivilet <levivilet@pm.me>
2026-03-26 17:51:57 +0100

    fix

--- packages/chat-view/src/parts/ChatMessageParsingRequest/ChatMessageParsingRequest.ts
+++ packages/chat-view/src/parts/ChatMessageParsingRequest/ChatMessageParsingRequest.ts
@@ -1,7 +1,6 @@
 import { ChatMessageParsingWorker } from '@lvce-editor/rpc-registry'
 import type { MessageIntermediateNode } from '../ParseMessageContentTypes/ParseMessageContentTypes.ts'
 
-<<<<<<< HEAD
 export const parseMessageContent = async (rawMessage: string): Promise<readonly MessageIntermediateNode[]> => {
   return ChatMessageParsingWorker.invoke('ChatMessageParsing.parseMessageContent', rawMessage) as Promise<readonly MessageIntermediateNode[]>
 }
@@ -10,11 +9,4 @@ export const parseMessageContents = async (rawMessages: readonly string[]): Prom
   return ChatMessageParsingWorker.invoke('ChatMessageParsing.parseMessageContents', rawMessages) as Promise<
     readonly (readonly MessageIntermediateNode[])[]
   >
-=======
-export const parseAndStoreMessagesContent = async (
-  parsedMessages: readonly ParsedMessage[],
-  messages: readonly ChatMessage[],
-): Promise<readonly ParsedMessage[]> => {
-  return ChatMessageParsingWorker.invoke('ChatParser.parseMessageContent', parsedMessages, messages) as Promise<readonly ParsedMessage[]>
->>>>>>> origin/main
 }
