e4640eec6355ba27b260901a2d9183cad22a887a
Le Vivilet <levivilet@pm.me>
2026-03-26 16:16:17 +0100

    feat: update @lvce-editor/rpc-registry to version 9.14.0 and fix sendMessagePortToChatMessageParsingWorker call (#817)

--- packages/chat-view/src/parts/InitializeChatMessageParsingWorker/InitializeChatMessageParsingWorker.ts
+++ packages/chat-view/src/parts/InitializeChatMessageParsingWorker/InitializeChatMessageParsingWorker.ts
@@ -2,8 +2,7 @@ import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { ChatMessageParsingWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 
 const send = (port: MessagePort): Promise<void> => {
-  // @ts-ignore
-  return RendererWorker.sendMessagePortToChatMessageParsingWorker(port)
+  return RendererWorker.sendMessagePortToChatMessageParsingWorker(port, 0)
 }
 
 export const initializeChatMessageParsingWorker = async (): Promise<void> => {
