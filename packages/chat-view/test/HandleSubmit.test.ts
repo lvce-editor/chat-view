0c5b94d06d8642104bd1949161b4818f2fd64255
Le Vivilet <levivilet@pm.me>
2026-03-26 11:45:12 +0100

    fix

--- packages/chat-view/test/HandleSubmit.test.ts
+++ packages/chat-view/test/HandleSubmit.test.ts
@@ -4,8 +4,8 @@ import { ChatToolWorker, ExtensionHost, RendererWorker } from '@lvce-editor/rpc-
 import { getChatViewEvents } from '../src/parts/ChatSessionStorage/ChatSessionStorage.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as HandleSubmit from '../src/parts/HandleSubmit/HandleSubmit.ts'
-import { registerMockChatStorageRpc } from '../src/parts/TestHelpers/RegisterMockChatStorageRpc.ts'
 import { registerSlashCommands } from '../src/parts/RegisterSlashCommands/RegisterSlashCommands.ts'
+import { registerMockChatStorageRpc } from '../src/parts/TestHelpers/RegisterMockChatStorageRpc.ts'
 
 registerSlashCommands()
 
