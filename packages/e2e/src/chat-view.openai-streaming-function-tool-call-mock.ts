a28bc50ba5101ceda517e649122a86ff407b0ca6
Le Vivilet <levivilet@pm.me>
2026-03-26 11:31:17 +0100

    e2e

--- packages/e2e/src/chat-view.openai-streaming-function-tool-call-mock.ts
+++ packages/e2e/src/chat-view.openai-streaming-function-tool-call-mock.ts
@@ -148,5 +148,5 @@ export const test: Test = async ({ Chat, Command, expect, FileSystem, Locator, W
   const messages = Locator('.ChatMessages .Message')
   await expect(messages).toHaveCount(2)
   await expect(messages.nth(0)).toHaveText('whats the contents of index html')
-  await expect(messages.nth(1)).toHaveText('toolsread_file index.html')
+  await expect(messages.nth(1)).toHaveText('toolsread_file index.html (error: Invalid argument: uri must be an absolute URI.)')
 }
