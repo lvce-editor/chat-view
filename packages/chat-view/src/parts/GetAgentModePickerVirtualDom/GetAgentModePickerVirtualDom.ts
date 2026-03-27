fa8455ad524d39d9ac6ba215ce2376ccc09db065
Le Vivilet <levivilet@pm.me>
2026-03-27 08:53:32 +0100

    feat: replace custom select toggle with picker toggle in various components (#834)

--- packages/chat-view/src/parts/GetAgentModePickerVirtualDom/GetAgentModePickerVirtualDom.ts
+++ packages/chat-view/src/parts/GetAgentModePickerVirtualDom/GetAgentModePickerVirtualDom.ts
@@ -1,22 +1,14 @@
-import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { getAgentModeLabel, type AgentMode } from '../AgentMode/AgentMode.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import { getCustomSelectToggleVirtualDom } from '../GetCustomSelectToggleVirtualDom/GetCustomSelectToggleVirtualDom.ts'
+import { getCustomSelectPickerToggleVirtualDom } from '../GetCustomSelectPickerToggleVirtualDom/GetCustomSelectPickerToggleVirtualDom.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const getAgentModePickerVirtualDom = (selectedAgentMode: AgentMode, agentModePickerOpen: boolean): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.CustomSelectContainer,
-      type: VirtualDomElements.Div,
-    },
-    ...getCustomSelectToggleVirtualDom(
-      getAgentModeLabel(selectedAgentMode),
-      InputName.AgentModePickerToggle,
-      agentModePickerOpen,
-      DomEventListenerFunctions.HandleClickAgentModePickerToggle,
-    ),
-  ]
+  return getCustomSelectPickerToggleVirtualDom(
+    getAgentModeLabel(selectedAgentMode),
+    InputName.AgentModePickerToggle,
+    agentModePickerOpen,
+    DomEventListenerFunctions.HandleClickAgentModePickerToggle,
+  )
 }
