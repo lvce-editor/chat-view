167d85a1fb38c2b057effabba3fbd439eb8cfff3
Le Vivilet <levivilet@pm.me>
2026-03-27 22:30:59 +0100

    feat: refactor agent mode picker to use separate function for options rendering

--- packages/chat-view/src/parts/GetAgentModePickerPopOverVirtualDom/GetAgentModePickerPopOverVirtualDom.ts
+++ packages/chat-view/src/parts/GetAgentModePickerPopOverVirtualDom/GetAgentModePickerPopOverVirtualDom.ts
@@ -1,20 +1,9 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { agentModes, getAgentModeLabel, type AgentMode } from '../AgentMode/AgentMode.ts'
-import { getCustomSelectOptionVirtualDom } from '../GetCustomSelectOptionVirtualDom/GetCustomSelectOptionVirtualDom.ts'
+import { agentModes, type AgentMode } from '../AgentMode/AgentMode.ts'
+import { getAgentModeOptionsVirtualDom } from '../GetAgentModeOptionsVirtualDom/GetAgentModeOptionsVirtualDom.ts'
 import { getCustomSelectPopOverVirtualDom } from '../GetCustomSelectPopOverVirtualDom/GetCustomSelectPopOverVirtualDom.ts'
-import * as InputName from '../InputName/InputName.ts'
 
 const agentModePickerHeight = agentModes.length * 28
-
-const getAgentModeOptionsVirtualDom = (selectedAgentMode: AgentMode): readonly VirtualDomNode[] => {
-  return agentModes.flatMap((agentMode) =>
-    getCustomSelectOptionVirtualDom(
-      InputName.getAgentModePickerItemInputName(agentMode),
-      getAgentModeLabel(agentMode),
-      agentMode === selectedAgentMode,
-    ),
-  )
-}
 
 export const getAgentModePickerPopOverVirtualDom = (selectedAgentMode: AgentMode): readonly VirtualDomNode[] => {
   return getCustomSelectPopOverVirtualDom(agentModes.length, agentModePickerHeight, getAgentModeOptionsVirtualDom(selectedAgentMode))
