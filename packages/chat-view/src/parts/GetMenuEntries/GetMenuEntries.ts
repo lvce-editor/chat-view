2526cea4c401f2014b347228ec3b0c528705b3d2
Le Vivilet <levivilet@pm.me>
2026-03-27 15:35:00 +0100

    feature: support image context menu (#846)

--- packages/chat-view/src/parts/GetMenuEntries/GetMenuEntries.ts
+++ packages/chat-view/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -1,14 +1,24 @@
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import type { ContextMenuProps } from './ContextMenuProps/ContextMenuProps.ts'
+import { getMenuEntriesChatAttachment } from '../GetMenuEntriesChatAttachment/GetMenuEntriesChatAttachment.ts'
 import { getMenuEntriesChatHeader } from '../GetMenuEntriesChatHeader/GetMenuEntriesChatHeader.ts'
 import { getMenuEntriesChatInput } from '../GetMenuEntriesChatInput/GetMenuEntriesChatInput.ts'
 import { getMenuEntriesChatList } from '../GetMenuEntriesChatList/GetMenuEntriesChatList.ts'
 import { getMenuEntriesChatProjectList } from '../GetMenuEntriesChatProjectList/GetMenuEntriesChatProjectList.ts'
 import { getMenuEntriesProjectAddButton } from '../GetMenuEntriesProjectAddButton/GetMenuEntriesProjectAddButton.ts'
-import { MenuChatHeader, MenuChatInput, MenuChatList, MenuChatProjectList, MenuProjectAddButton } from '../GetMenuEntryIds/GetMenuEntryIds.ts'
+import {
+  MenuChatAttachment,
+  MenuChatHeader,
+  MenuChatInput,
+  MenuChatList,
+  MenuChatProjectList,
+  MenuProjectAddButton,
+} from '../GetMenuEntryIds/GetMenuEntryIds.ts'
 
 export const getMenuEntries = (menuId: number, props: ContextMenuProps): readonly MenuEntry[] => {
   switch (props.menuId) {
+    case MenuChatAttachment:
+      return getMenuEntriesChatAttachment(props.attachmentId, props.previewSrc)
     case MenuChatHeader:
       return getMenuEntriesChatHeader()
     case MenuChatInput:
