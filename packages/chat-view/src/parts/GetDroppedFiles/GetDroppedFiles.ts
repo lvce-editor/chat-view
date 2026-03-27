83afa3c4c5c92e52a86269b8c37f9d48da95ce46
Le Vivilet <levivilet@pm.me>
2026-03-27 13:15:47 +0100

    fix

--- packages/chat-view/src/parts/GetDroppedFiles/GetDroppedFiles.ts
+++ packages/chat-view/src/parts/GetDroppedFiles/GetDroppedFiles.ts
@@ -5,6 +5,9 @@ interface FileHandleTransportItem {
 }
 
 export const getDroppedFiles = async (fileHandles: readonly number[]): Promise<readonly FileSystemFileHandle[]> => {
+  if (fileHandles.some((item) => typeof item !== 'number')) {
+    return fileHandles as any as readonly FileSystemFileHandle[]
+  }
   const actualHandles = await RendererWorker.getFileHandles(fileHandles)
   return actualHandles
     .map((item) => (item as unknown as FileHandleTransportItem).value)
