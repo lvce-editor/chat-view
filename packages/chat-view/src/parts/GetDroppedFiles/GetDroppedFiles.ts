2064e0f05f4d838f1758afc0307c90ab36ee1593
Le Vivilet <levivilet@pm.me>
2026-03-27 15:10:04 +0100

    fix

--- packages/chat-view/src/parts/GetDroppedFiles/GetDroppedFiles.ts
+++ packages/chat-view/src/parts/GetDroppedFiles/GetDroppedFiles.ts
@@ -6,7 +6,7 @@ interface FileHandleTransportItem {
 
 export const getDroppedFiles = async (fileHandles: readonly number[]): Promise<readonly FileSystemFileHandle[]> => {
   // TODO adjust e2e test and remove this code
-  if (fileHandles.some((item) => typeof item !== 'number')) {
+  if (fileHandles.some((item: any): boolean => typeof item !== 'number')) {
     return fileHandles.map((item: any): any => {
       return {
         getFile() {
