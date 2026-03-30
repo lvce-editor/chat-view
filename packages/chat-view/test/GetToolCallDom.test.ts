0c182f90f67e4362c7f2fed59874a05ee5107e10
Le Vivilet <levivilet@pm.me>
2026-03-30 14:27:12 +0200

    fix

--- packages/chat-view/test/GetToolCallDom.test.ts
+++ packages/chat-view/test/GetToolCallDom.test.ts
@@ -228,10 +228,6 @@ test('getToolCallDom should render finished glob tool calls with a match count',
     {
       childCount: 4,
       className: ClassNames.ChatOrderedListItem,
-<<<<<<< HEAD
-=======
-      title: baseUri,
->>>>>>> origin/main
       type: VirtualDomElements.Li,
     },
     {
