3044700d318d0f030d64051914f20d3921bdbc0c
Le Vivilet <levivilet@pm.me>
2026-03-30 14:20:25 +0200

    fix

--- packages/e2e/src/chat-view.focus-header.ts
+++ packages/e2e/src/chat-view.focus-header.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.focus-header'
 
-export const skip=1
+export const skip = 1
 
 export const test: Test = async ({ Chat, Command, expect, Locator }) => {
   await Chat.show()
