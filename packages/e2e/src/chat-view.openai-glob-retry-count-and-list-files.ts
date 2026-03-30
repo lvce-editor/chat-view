1b9f1d177cf900fdb861d5d1f23d545f202a7b19
Le Vivilet <levivilet@pm.me>
2026-03-30 17:09:44 +0200

    e2e

--- packages/e2e/src/chat-view.openai-glob-retry-count-and-list-files.ts
+++ packages/e2e/src/chat-view.openai-glob-retry-count-and-list-files.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.openai-glob-retry-count-and-list-files'
 
+export const skip=1
+
 export const test: Test = async ({ Chat, Command, expect, Locator }) => {
   const workspaceUri = 'file:///playground'
   const files = [
