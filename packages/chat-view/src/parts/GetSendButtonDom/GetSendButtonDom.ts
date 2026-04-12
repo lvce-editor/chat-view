1a3c5c1e2ecbf896030d7add9b5fa53df8f3513f
Le Vivilet <levivilet@pm.me>
2026-04-12 18:23:06 +0200

    feature: add dictation, stop, and submit button DOM generation functions

--- packages/chat-view/src/parts/GetSendButtonDom/GetSendButtonDom.ts
+++ packages/chat-view/src/parts/GetSendButtonDom/GetSendButtonDom.ts
@@ -1,63 +1,15 @@
-import { type VirtualDomNode, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import * as Strings from '../ChatStrings/ChatStrings.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import { getSendButtonClassName } from '../GetSendButtonClassName/GetSendButtonClassName.ts'
-import * as InputName from '../InputName/InputName.ts'
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { getDictationButtonDom } from '../GetDictationButtonDom/GetDictationButtonDom.ts'
+import { getStopButtonDom } from '../GetStopButtonDom/GetStopButtonDom.ts'
+import { getSubmitButtonDom } from '../GetSubmitButtonDom/GetSubmitButtonDom.ts'
 
 export const getSendButtonDom = (
   isSendDisabled: boolean,
   voiceDictationEnabled: boolean,
   isSessionInProgress: boolean,
 ): readonly VirtualDomNode[] => {
-  const sendButtonClassName = getSendButtonClassName(isSendDisabled)
   return [
-    ...(voiceDictationEnabled
-      ? [
-          {
-            childCount: 1,
-            className: ClassNames.IconButton,
-            name: InputName.Dictate,
-            onClick: DomEventListenerFunctions.HandleClickDictationButton,
-            title: Strings.startVoiceDictation(),
-            type: VirtualDomElements.Button,
-          },
-          {
-            childCount: 0,
-            className: 'MaskIcon MaskIconMic',
-            type: VirtualDomElements.Div,
-          },
-        ]
-      : []),
-    ...(isSessionInProgress
-      ? [
-          {
-            buttonType: 'button',
-            childCount: 1,
-            className: ClassNames.Button,
-            name: InputName.Stop,
-            onClick: DomEventListenerFunctions.HandleClick,
-            title: Strings.stop(),
-            type: VirtualDomElements.Button,
-          },
-          text(Strings.stop()),
-        ]
-      : [
-          {
-            buttonType: 'submit',
-            childCount: 1,
-            className: sendButtonClassName,
-            disabled: isSendDisabled,
-            name: InputName.Send,
-            title: Strings.sendMessage(),
-            type: VirtualDomElements.Button,
-          },
-          {
-            childCount: 0,
-            className: 'MaskIcon MaskIconArrowUp',
-            role: 'none',
-            type: VirtualDomElements.Div,
-          },
-        ]),
+    ...(voiceDictationEnabled ? getDictationButtonDom() : []),
+    ...(isSessionInProgress ? getStopButtonDom() : getSubmitButtonDom(isSendDisabled)),
   ]
 }
