376ae057f584ce0653ecf97790b5faa7dbbd993a
Le Vivilet <levivilet@pm.me>
2026-04-14 15:51:44 +0200

    skip

--- packages/e2e/src/chat-view.switch-chat-existing-messages.ts
+++ packages/e2e/src/chat-view.switch-chat-existing-messages.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.switch-chat-existing-messages'
 
+export const skip = 1
+
 export const test: Test = async ({ Chat, Command, expect, Locator }) => {
   await Chat.show()
   await Chat.reset()
