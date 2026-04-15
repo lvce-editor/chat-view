47d140c60e1ea617bab767e00d9e1f935c1d240b
Le Vivilet <levivilet@pm.me>
2026-04-15 13:10:02 +0200

    fix

--- packages/chat-view/src/parts/SetRenderSelectChevrons/SetRenderSelectChevrons.ts
+++ packages/chat-view/src/parts/SetRenderSelectChevrons/SetRenderSelectChevrons.ts
@@ -5,18 +5,4 @@ export const setRenderSelectChevrons = (state: ChatState, renderSelectChevrons: 
     ...state,
     renderSelectChevrons,
   }
-}import type { ChatState } from '../ChatState/ChatState.ts'
-
-export const setRenderSelectChevrons = (state: ChatState, renderSelectChevrons: boolean): ChatState => {
-  return {
-    ...state,
-    renderSelectChevrons,
-  }
-}import type { ChatState } from '../ChatState/ChatState.ts'
-
-export const setRenderSelectChevrons = (state: ChatState, renderSelectChevrons: boolean): ChatState => {
-  return {
-    ...state,
-    renderSelectChevrons,
-  }
 }
