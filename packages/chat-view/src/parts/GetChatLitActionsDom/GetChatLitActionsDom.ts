c3ad079e0a01cda0752cd7f9ef264552138ca526
Le Vivilet <levivilet@pm.me>
2026-04-15 15:03:15 +0200

    fix

---
+++ packages/chat-view/src/parts/GetChatLitActionsDom/GetChatLitActionsDom.ts
@@ -0,0 +1,55 @@
+import { AriaRoles, type VirtualDomNode, mergeClassNames, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { ChatSession } from '../ChatSession/ChatSession.ts'
+import * as Strings from '../ChatStrings/ChatStrings.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as InputName from '../InputName/InputName.ts'
+
+export const getChatLitActionsDom = (session: ChatSession, sessionPinningEnabled = true): readonly VirtualDomNode[] => {
+  return [
+    {
+      childCount: sessionPinningEnabled ? 2 : 1,
+      className: ClassNames.ChatActions,
+      role: AriaRoles.ToolBar,
+      type: VirtualDomElements.Div,
+    },
+    ...(sessionPinningEnabled
+      ? [
+          {
+            childCount: 1,
+            className: mergeClassNames(
+              ClassNames.IconButton,
+              ClassNames.SessionPinButton,
+              session.pinned ? ClassNames.ChatListItemPinned : ClassNames.Empty,
+            ),
+            'data-id': session.id,
+            name: InputName.SessionPin,
+            onClick: DomEventListenerFunctions.HandleClickPin,
+            tabIndex: 0,
+            title: session.pinned ? Strings.unpinChatSession() : Strings.pinChatSession(),
+            type: VirtualDomElements.Button,
+          },
+          {
+            childCount: 0,
+            className: mergeClassNames(ClassNames.MaskIcon, ClassNames.MaskIconPinned),
+            type: VirtualDomElements.Div,
+          },
+        ]
+      : []),
+    {
+      childCount: 1,
+      className: mergeClassNames(ClassNames.IconButton, ClassNames.SessionArchiveButton),
+      'data-id': session.id,
+      name: InputName.SessionDelete,
+      onClick: DomEventListenerFunctions.HandleClickDelete,
+      tabIndex: 0,
+      title: Strings.deleteChatSession(),
+      type: VirtualDomElements.Button,
+    },
+    {
+      childCount: 0,
+      className: mergeClassNames(ClassNames.MaskIcon, ClassNames.MaskIconArchive),
+      type: VirtualDomElements.Div,
+    },
+  ]
+}
--- packages/chat-view/src/parts/GetSessionDom/GetSessionDom.ts
+++ packages/chat-view/src/parts/GetSessionDom/GetSessionDom.ts
@@ -1,12 +1,19 @@
-import { AriaRoles, type VirtualDomNode, mergeClassNames, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
+import { type VirtualDomNode, mergeClassNames, VirtualDomElements, text } from '@lvce-editor/virtual-dom-worker'
 import type { ChatSession } from '../ChatSession/ChatSession.ts'
-import * as Strings from '../ChatStrings/ChatStrings.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { formatChatListTime } from '../FormatChatListTime/FormatChatListTime.ts'
 import { getSessionLastActiveTime } from '../GetSessionLastActiveTime/GetSessionLastActiveTime.ts'
 import { getSessionStatusClassName } from '../GetSessionStatusClassName/GetSessionStatusClassName.ts'
 import * as InputName from '../InputName/InputName.ts'
+
+const getSessionClassname = (focused: boolean, showFocusOutline: boolean): string => {
+  return showFocusOutline
+    ? mergeClassNames(ClassNames.ChatListItem, ClassNames.ChatListItemFocused, ClassNames.ChatListItemFocusOutline, ClassNames.FocusOutline)
+    : focused
+      ? mergeClassNames(ClassNames.ChatListItem, ClassNames.ChatListItemFocused)
+      : ClassNames.ChatListItem
+}
 
 export const getSessionDom = (
   session: ChatSession,
@@ -15,11 +22,7 @@ export const getSessionDom = (
   showFocusOutline = false,
   sessionPinningEnabled = true,
 ): readonly VirtualDomNode[] => {
-  const sessionClassName = showFocusOutline
-    ? mergeClassNames(ClassNames.ChatListItem, ClassNames.ChatListItemFocused, ClassNames.ChatListItemFocusOutline, ClassNames.FocusOutline)
-    : focused
-      ? mergeClassNames(ClassNames.ChatListItem, ClassNames.ChatListItemFocused)
-      : ClassNames.ChatListItem
+  const sessionClassName = getSessionClassname(focused, showFocusOutline)
   const sessionStatusClassName = getSessionStatusClassName(session)
   const lastActiveTime = getSessionLastActiveTime(session)
   const formattedLastActiveTime = lastActiveTime ? formatChatListTime(lastActiveTime) : 'n/a'
@@ -70,49 +73,6 @@ export const getSessionDom = (
           text(formattedLastActiveTime),
         ]
       : []),
-    {
-      childCount: sessionPinningEnabled ? 2 : 1,
-      className: ClassNames.ChatActions,
-      role: AriaRoles.ToolBar,
-      type: VirtualDomElements.Div,
-    },
-    ...(sessionPinningEnabled
-      ? [
-          {
-            childCount: 1,
-            className: mergeClassNames(
-              ClassNames.IconButton,
-              ClassNames.SessionPinButton,
-              session.pinned ? ClassNames.ChatListItemPinned : ClassNames.Empty,
-            ),
-            'data-id': session.id,
-            name: InputName.SessionPin,
-            onClick: DomEventListenerFunctions.HandleClickPin,
-            tabIndex: 0,
-            title: session.pinned ? Strings.unpinChatSession() : Strings.pinChatSession(),
-            type: VirtualDomElements.Button,
-          },
-          {
-            childCount: 0,
-            className: mergeClassNames(ClassNames.MaskIcon, ClassNames.MaskIconPinned),
-            type: VirtualDomElements.Div,
-          },
-        ]
-      : []),
-    {
-      childCount: 1,
-      className: mergeClassNames(ClassNames.IconButton, ClassNames.SessionArchiveButton),
-      'data-id': session.id,
-      name: InputName.SessionDelete,
-      onClick: DomEventListenerFunctions.HandleClickDelete,
-      tabIndex: 0,
-      title: Strings.deleteChatSession(),
-      type: VirtualDomElements.Button,
-    },
-    {
-      childCount: 0,
-      className: mergeClassNames(ClassNames.MaskIcon, ClassNames.MaskIconArchive),
-      type: VirtualDomElements.Div,
-    },
+    ...getChatLitActionsDom(session, sessionPinningEnabled),
   ]
 }
