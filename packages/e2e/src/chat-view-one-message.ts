ccf682f634d280118f024d1f103defe04aebdb7f
Le Vivilet <levivilet@pm.me>
2026-04-23 10:37:01 +0200

    remove unused skip export from chat-view.one-message test (#1018)

--- packages/e2e/src/chat-view-one-message.ts
+++ packages/e2e/src/chat-view-one-message.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.one-message'
 
-export const skip = 1
-
 export const test: Test = async ({ Chat, expect, Locator }) => {
   // arrange
   await Chat.show()
