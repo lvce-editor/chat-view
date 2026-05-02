80dbf8297b0236b64f1eb1e9692a2c746271d550
Le Vivilet <levivilet@pm.me>
2026-05-02 12:40:39 +0000

    fix

--- packages/chat-view/src/parts/MockBackendSetRequestFailedResponse/MockBackendSetRequestFailedResponse.ts
+++ packages/chat-view/src/parts/MockBackendSetRequestFailedResponse/MockBackendSetRequestFailedResponse.ts
@@ -1,11 +1,7 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
 import * as MockBackendCompletion from '../MockBackendCompletion/MockBackendCompletion.ts'
 
-export const mockBackendSetRequestFailedResponse = (
-  state: ChatState,
-  errorMessage?: string,
-  errorCode: string = 'network_error',
-): ChatState => {
+export const mockBackendSetRequestFailedResponse = (state: ChatState, errorMessage?: string, errorCode: string = 'network_error'): ChatState => {
   MockBackendCompletion.setRequestFailedResponse(errorMessage, errorCode)
   return state
 }
