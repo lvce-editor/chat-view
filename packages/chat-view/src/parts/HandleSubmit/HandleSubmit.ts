7bad9dc8f845f3ffc59c37f5e59eaba287b1fccc
Le Vivilet <levivilet@pm.me>
2026-05-04 21:12:10 +0200

    fix

--- packages/chat-view/src/parts/HandleSubmit/HandleSubmit.ts
+++ packages/chat-view/src/parts/HandleSubmit/HandleSubmit.ts
@@ -2,5 +2,6 @@ import { ChatViewModelWorker } from '@lvce-editor/rpc-registry'
 import type { ChatState } from '../ChatState/ChatState.ts'
 
 export const handleSubmit = async (state: ChatState): Promise<ChatState> => {
-  return ChatViewModelWorker.invoke('ChatModel.handleSubmit', state) as Promise<ChatState>
+  await ChatViewModelWorker.invoke('ChatModel.handleSubmit', state)
+  return state
 }
