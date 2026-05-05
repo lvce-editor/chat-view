0fa0fa0f120700dd44562d6f684b28bc0d4b2d41
Le Vivilet <levivilet@pm.me>
2026-05-05 22:18:43 +0200

    fix

--- packages/e2e/src/chat-view.chat-list-context-menu-rename.ts
+++ packages/e2e/src/chat-view.chat-list-context-menu-rename.ts
@@ -6,7 +6,6 @@ export const test: Test = async ({ Chat, ContextMenu, expect, FileSystem, Locato
   const tmpDir = await FileSystem.getTmpDir()
   await Workspace.setPath(tmpDir)
   await Chat.show()
-  await Chat.reset()
   await Chat.handleInput('hello from e2e')
   await Chat.handleSubmit()
   await Chat.handleClickBack()
