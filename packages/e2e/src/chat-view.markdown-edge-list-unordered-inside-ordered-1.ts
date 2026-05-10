fe6d9c93bdc2fbe69cea5368d7f36e49f7e364d7
Le Vivilet <levivilet@pm.me>
2026-05-11 00:31:45 +0200

    feature: use view-model (#1066)

--- packages/e2e/src/chat-view.markdown-edge-list-unordered-inside-ordered-1.ts
+++ packages/e2e/src/chat-view.markdown-edge-list-unordered-inside-ordered-1.ts
@@ -1,5 +1,7 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
+export const skip = 1
+
 export const name = 'chat-view.markdown-edge.list-unordered-inside-ordered-1'
 
 export const test: Test = async ({ Chat, Command, expect, FileSystem, Locator, Workspace }) => {
