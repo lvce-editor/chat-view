4433da9b4b14fe0db5eb398254623c9052a59b87
Le Vivilet <levivilet@pm.me>
2026-05-10 22:48:51 +0200

    fix

--- packages/chat-view/src/parts/DiffSelection/DiffSelection.ts
+++ packages/chat-view/src/parts/DiffSelection/DiffSelection.ts
@@ -1,7 +1,7 @@
 import type { ChatState } from '../ChatState/ChatState.ts'
 
 export const diffSelection = (oldState: ChatState, newState: ChatState): boolean => {
-  if (newState.inputSource === 'script') {
+  if (newState.inputSource === 'user') {
     return true
   }
   return oldState.composerSelectionStart === newState.composerSelectionStart && oldState.composerSelectionEnd === newState.composerSelectionEnd
