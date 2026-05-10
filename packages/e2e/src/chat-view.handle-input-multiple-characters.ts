c37fc0c420eefb4c6771ddea63ebc3689610eede
Le Vivilet <levivilet@pm.me>
2026-05-10 22:58:15 +0200

    fix

--- packages/e2e/src/chat-view.handle-input-multiple-characters.ts
+++ packages/e2e/src/chat-view.handle-input-multiple-characters.ts
@@ -2,11 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.handle-input-multiple-characters'
 
-<<<<<<< HEAD
-=======
 export const skip = 1
 
->>>>>>> origin/main
 export const test: Test = async ({ Chat, expect, Locator }) => {
   // arrange
   await Chat.show()
@@ -15,15 +12,9 @@ export const test: Test = async ({ Chat, expect, Locator }) => {
 
   // act
   await Chat.handleInput('a')
-<<<<<<< HEAD
   await Chat.handleInput('ab')
   await Chat.handleInput('abc')
   await Chat.handleInput('abcd')
-=======
-  await Chat.handleInput('b')
-  await Chat.handleInput('c')
-  await Chat.handleInput('d')
->>>>>>> origin/main
 
   // assert
   await expect(composer).toHaveValue('abcd')
