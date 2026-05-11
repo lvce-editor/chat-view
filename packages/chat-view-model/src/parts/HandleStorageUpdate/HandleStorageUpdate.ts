6dc5da343eb7b609377e75243eeee6c3ede8605b
Le Vivilet <levivilet@pm.me>
2026-05-11 18:14:13 +0200

    fix

--- packages/chat-view-model/src/parts/HandleStorageUpdate/HandleStorageUpdate.ts
+++ packages/chat-view-model/src/parts/HandleStorageUpdate/HandleStorageUpdate.ts
@@ -1,61 +1,7 @@
 import { ChatStorageWorker, RendererWorker } from '@lvce-editor/rpc-registry'
-import type { PrototypeState, PrototypeStateBase } from '../PrototypeState/PrototypeState.ts'
-import type { ChatSession } from '../ViewModel/ViewModel.ts'
-import { listChatSessions } from '../ChatSessionStorage/ChatSessionStorage.ts'
-import { loadSelectedSessionMessages } from '../LoadSelectedSessionMessages/LoadSelectedSessionMessages.ts'
 import { getState, setState } from '../ModelState/ModelState.ts'
-import { normalizeSessionsOnLoad } from '../NormalizeSessionsOnLoad/NormalizeSessionsOnLoad.ts'
 import { parseAndStoreMessagesContent } from '../ParsedMessageContent/ParsedMessageContent.ts'
-
-const getTargetSessionId = (
-  state: {
-    readonly lastSubmittedSessionId?: string
-    readonly selectedSessionId: string
-    readonly sessions: readonly ChatSession[]
-    readonly viewMode: string
-  },
-  sessionId: string,
-): string => {
-  if (state.viewMode === 'list') {
-    return state.lastSubmittedSessionId || sessionId
-  }
-  if (state.selectedSessionId === sessionId) {
-    return state.selectedSessionId
-  }
-  const selectedSession = state.sessions.find((session) => session.id === state.selectedSessionId)
-  if (!selectedSession || selectedSession.messages.length === 0) {
-    return sessionId
-  }
-  return state.selectedSessionId || sessionId
-}
-
-const shouldSwitchToDetailMode = (state: { readonly viewMode: string }, selectedSession?: ChatSession): boolean => {
-  return state.viewMode === 'list' && !!selectedSession && selectedSession.messages.length > 0
-}
-
-export const getNextStateFromStorageUpdate = async (state: Readonly<PrototypeStateBase>, sessionId: string): Promise<PrototypeState> => {
-  // TODO depending on list or detail view, need to load either
-  // a list of chat sessions
-  // or a list of chat messages for that sessionId
-  const selectedSessionId = getTargetSessionId(state, sessionId)
-  let sessions = (await listChatSessions()) as readonly ChatSession[]
-  sessions = await loadSelectedSessionMessages(sessions, selectedSessionId)
-  sessions = normalizeSessionsOnLoad(sessions)
-  const { parsedMessages: previousParsedMessages } = state
-
-  let parsedMessages = previousParsedMessages
-  for (const session of sessions) {
-    parsedMessages = await parseAndStoreMessagesContent(parsedMessages, session.messages)
-  }
-  const selectedSession = sessions.find((session) => session.id === selectedSessionId)
-  return {
-    ...state,
-    parsedMessages,
-    selectedSessionId: selectedSession?.id || selectedSessionId,
-    sessions,
-    viewMode: shouldSwitchToDetailMode(state, selectedSession) ? 'detail' : state.viewMode,
-  }
-}
+import type { PrototypeStateBase } from '../PrototypeState/PrototypeState.ts'
 
 const handleStorageUpdateListMode = async (state: PrototypeStateBase): Promise<PrototypeStateBase> => {
   // TODO update / requery list
