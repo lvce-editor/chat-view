206aa07d2bc8847e95a2766a930e01f273587d90
Le Vivilet <levivilet@pm.me>
2026-05-11 17:32:53 +0200

    refactor: remove unused skip constant from chat-view submit message test (#1114)

--- packages/e2e/src/chat-view.submit-message-preserves-new-lines.ts
+++ packages/e2e/src/chat-view.submit-message-preserves-new-lines.ts
@@ -1,7 +1,5 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const skip = 1
-
 export const name = 'chat-view.submit-message-preserves-new-lines'
 
 export const test: Test = async ({ Chat, expect, Locator }) => {
