bb94931fbcbc575a27be9851da290b81cee2ab13
Le Vivilet <levivilet@pm.me>
2026-05-13 12:54:30 +0200

    refactor: remove unused skip constants from e2e test files and update mockOpenApiStream functions to delegate correctly (#1115)

--- packages/chat-view/test/MockOpenApiStreamPushChunk.test.ts
+++ packages/chat-view/test/MockOpenApiStreamPushChunk.test.ts
@@ -1,11 +1,28 @@
 import { expect, test } from '@jest/globals'
+import { ChatCoordinatorWorker } from '@lvce-editor/rpc-registry'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { mockOpenApiStreamPushChunk } from '../src/parts/MockOpenApiStreamPushChunk/MockOpenApiStreamPushChunk.ts'
 
 test('mockOpenApiStreamPushChunk should delegate to chat coordinator worker', async () => {
+  using mockRpc = ChatCoordinatorWorker.registerMockRpc({
+    'ChatCoordinator.mockOpenApiStreamPushChunk': async () => {},
+  })
   const state = createDefaultState()
 
   const result = await mockOpenApiStreamPushChunk(state, 'chunk-1')
 
   expect(result).toBe(state)
+  expect(mockRpc.invocations).toEqual([['ChatCoordinator.mockOpenApiStreamPushChunk', 'chunk-1']])
+})
+
+test('mockOpenApiStreamPushChunk should not delegate named requests to chat coordinator worker', async () => {
+  using mockRpc = ChatCoordinatorWorker.registerMockRpc({
+    'ChatCoordinator.mockOpenApiStreamPushChunk': async () => {},
+  })
+  const state = createDefaultState()
+
+  const result = await mockOpenApiStreamPushChunk(state, 'request-1', 'chunk-1')
+
+  expect(result).toBe(state)
+  expect(mockRpc.invocations).toEqual([])
 })
