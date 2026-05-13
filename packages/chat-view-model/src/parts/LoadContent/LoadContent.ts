dce768e4f62ee917fa75eddb62fc3f29c7b240ed
Le Vivilet <levivilet@pm.me>
2026-05-13 23:31:24 +0200

    refactor: remove legacy session handling from loadContent function (#1123)

--- packages/chat-view-model/src/parts/LoadContent/LoadContent.ts
+++ packages/chat-view-model/src/parts/LoadContent/LoadContent.ts
@@ -2,7 +2,7 @@
 
 import type { ChatSession, ChatViewMode, ViewModel } from '../ViewModel/ViewModel.ts'
 import { getLoggedOutBackendAuthState, syncBackendAuth } from '../BackendAuth/BackendAuth.ts'
-import { listChatSessions, saveChatSession, subscribeSessionUpdates } from '../ChatSessionStorage/ChatSessionStorage.ts'
+import { listChatSessions, subscribeSessionUpdates } from '../ChatSessionStorage/ChatSessionStorage.ts'
 import { ensureBlankProject } from '../EnsureBlankProject/EnsureBlankProject.ts'
 import { getComposerAttachments } from '../GetComposerAttachments/GetComposerAttachments.ts'
 import { getComposerAttachmentsHeight } from '../GetComposerAttachmentsHeight/GetComposerAttachmentsHeight.ts'
@@ -21,7 +21,6 @@ import { getSavedReasoningEffort } from '../GetSavedReasoningEffort/GetSavedReas
 import { getSavedSelectedModelId } from '../GetSavedSelectedModelId/GetSavedSelectedModelId.ts'
 import { getSavedSelectedProjectId } from '../GetSavedSelectedProjectId/GetSavedSelectedProjectId.ts'
 import { getSavedSelectedSessionId } from '../GetSavedSelectedSessionId/GetSavedSelectedSessionId.ts'
-import { getSavedSessions } from '../GetSavedSessions/GetSavedSessions.ts'
 import { getSavedViewMode } from '../GetSavedViewMode/GetSavedViewMode.ts'
 import { getVisibleModels } from '../GetVisibleModels/GetVisibleModels.ts'
 import { getVisibleSessions } from '../GetVisibleSessions/GetVisibleSessions.ts'
@@ -31,7 +30,6 @@ import { setState, setSubscribedSessionId } from '../ModelState/ModelState.ts'
 import { normalizeSessionsOnLoad } from '../NormalizeSessionsOnLoad/NormalizeSessionsOnLoad.ts'
 import { parseAndStoreMessagesContent } from '../ParsedMessageContent/ParsedMessageContent.ts'
 import { refreshGitBranchPickerVisibility } from '../RefreshGitBranchPickerVisibility/RefreshGitBranchPickerVisibility.ts'
-import { toSummarySession } from '../ToSummarySession/ToSummarySession.ts'
 
 export type LastNormalViewMode = Extract<ChatViewMode, 'list' | 'detail'>
 
@@ -111,21 +109,8 @@ export const loadContent = async <TState extends LoadContentState>(state: TState
   } = await loadPreferences()
   const authState =
     authEnabled || useOwnBackend ? (backendUrl ? await syncBackendAuth(backendUrl) : getLoggedOutBackendAuthState()) : getLoggedOutBackendAuthState()
-  const legacySavedSessions = getSavedSessions(savedState)
   const storedSessions = await listChatSessions()
   let sessions: readonly ChatSession[] = storedSessions
-  if (sessions.length === 0 && legacySavedSessions && legacySavedSessions.length > 0) {
-    for (const session of legacySavedSessions) {
-      await saveChatSession(session as Parameters<typeof saveChatSession>[0])
-    }
-    sessions = legacySavedSessions.map(toSummarySession)
-  }
-  if (sessions.length === 0 && state.sessions.length > 0) {
-    for (const session of state.sessions) {
-      await saveChatSession(session as Parameters<typeof saveChatSession>[0])
-    }
-    sessions = state.sessions.map(toSummarySession)
-  }
   const preferredSessionId = getSavedSelectedSessionId(savedState) || state.selectedSessionId
   const savedProjects = getSavedProjects(savedState)
   const baseProjects = savedProjects && savedProjects.length > 0 ? savedProjects : state.projects
