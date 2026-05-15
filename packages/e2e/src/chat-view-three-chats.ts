414544d08e895750b75984802017463f4a48c70f
Le Vivilet <levivilet@pm.me>
2026-05-15 23:31:09 +0200

    fix: update expected title in end-to-end chat view test (#1141)

--- packages/e2e/src/chat-view-three-chats.ts
+++ packages/e2e/src/chat-view-three-chats.ts
@@ -14,10 +14,14 @@ export const test: Test = async ({ Chat, expect, Locator }) => {
   await Chat.handleSubmit()
   await Chat.handleClickBack()
 
+  await Chat.handleInput('third chat')
+  await Chat.handleSubmit()
+  await Chat.handleClickBack()
+
   // assert
   const item = Locator('.ChatListItem')
-  await expect(item).toHaveCount(2)
-  const secondItem = item.nth(1)
-  const secondItemTitle = secondItem.locator('.ChatListItemTitle')
-  await expect(secondItemTitle).toHaveText('second chat')
+  await expect(item).toHaveCount(3)
+  const thirdItem = item.nth(2)
+  const thirdItemTitle = thirdItem.locator('.ChatListItemTitle')
+  await expect(thirdItemTitle).toHaveText('third chat')
 }
