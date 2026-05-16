71bac8509aeb85a0209fa966d4b02426c2bb982d
Le Vivilet <levivilet@pm.me>
2026-05-16 20:23:05 +0000

    menu

--- packages/chat-view/src/parts/GetSessionClassName/GetSessionClassName.ts
+++ packages/chat-view/src/parts/GetSessionClassName/GetSessionClassName.ts
@@ -4,7 +4,13 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 export const getSessionClassName = (focused: boolean, showFocusOutline: boolean, unread = false): string => {
   const unreadClassName = unread ? ClassNames.ChatListItemUnread : ClassNames.Empty
   if (showFocusOutline) {
-    return mergeClassNames(ClassNames.ChatListItem, ClassNames.ChatListItemFocused, ClassNames.ChatListItemFocusOutline, ClassNames.FocusOutline, unreadClassName)
+    return mergeClassNames(
+      ClassNames.ChatListItem,
+      ClassNames.ChatListItemFocused,
+      ClassNames.ChatListItemFocusOutline,
+      ClassNames.FocusOutline,
+      unreadClassName,
+    )
   }
   if (focused) {
     return mergeClassNames(ClassNames.ChatListItem, ClassNames.ChatListItemFocused, unreadClassName)
