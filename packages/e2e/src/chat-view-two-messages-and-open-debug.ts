7ce7b567b26f74b0d3737106c43902937be4e9c0
Le Vivilet <levivilet@pm.me>
2026-05-17 09:18:33 +0200

    feature: remove unused skip export from chat-view test (#1153)

--- packages/e2e/src/chat-view-two-messages-and-open-debug.ts
+++ packages/e2e/src/chat-view-two-messages-and-open-debug.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.two-messages-and-open-debug'
 
-export const skip = 1
-
 export const test: Test = async ({ Chat, Command, expect, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
