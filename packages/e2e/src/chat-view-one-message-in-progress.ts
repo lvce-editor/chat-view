47f0f5eada520a95bf077d3ec6e961acb4813203
Le Vivilet <levivilet@pm.me>
2026-05-24 17:42:28 +0000

    fix

--- packages/e2e/src/chat-view-one-message-in-progress.ts
+++ packages/e2e/src/chat-view-one-message-in-progress.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat-view.one-message-in-progress'
 
-export const test: Test = async ({ Chat, expect, Locator, Command }) => {
+export const test: Test = async ({ Chat, Command, expect, Locator }) => {
   // arrange
   await Chat.show()
   await Chat.handleInput('hello from e2e')
