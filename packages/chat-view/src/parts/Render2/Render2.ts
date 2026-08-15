ea961c194e6b063862dd17016ee5c12a5020b961
Le Vivilet <levivilet@pm.me>
2026-08-15 06:41:42 -0700

    Render directly in renderer process (#1264)

--- packages/chat-view/src/parts/Render2/Render2.ts
+++ packages/chat-view/src/parts/Render2/Render2.ts
@@ -1,9 +1,18 @@
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
+import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 import * as SourceControlStates from '../StatusBarStates/StatusBarStates.ts'
 
-export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] => {
+export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] | Promise<readonly any[]> => {
   const { newState, oldState } = SourceControlStates.get(uid)
   SourceControlStates.set(uid, newState, newState)
   const commands = ApplyRender.applyRender(oldState, newState, diffResult)
-  return commands
+  if (!RendererProcess.isConnected()) return commands
+  return renderDirect(uid, commands)
+}
+
+const renderDirect = async (uid: number, commands: readonly any[]): Promise<readonly any[]> => {
+  const rendererWorkerCommands = commands.filter((command) => command[0] === 'Viewlet.setFocusContext')
+  const rendererProcessCommands = commands.filter((command) => command[0] !== 'Viewlet.setFocusContext')
+  const transactionId = await RendererProcess.invoke('Viewlet.queueCommands', uid, rendererProcessCommands)
+  return [...rendererWorkerCommands, ['Viewlet.commitPending', uid, transactionId]]
 }
