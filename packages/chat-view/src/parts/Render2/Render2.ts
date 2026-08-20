3f07b6af426a729a1893ff07d54cd7c0e79f30b5
Le Vivilet <levivilet@pm.me>
2026-08-20 22:11:08 +0000

    fix: scope direct focus commands to chat view

--- packages/chat-view/src/parts/Render2/Render2.ts
+++ packages/chat-view/src/parts/Render2/Render2.ts
@@ -2,6 +2,13 @@ import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 import * as SourceControlStates from '../StatusBarStates/StatusBarStates.ts'
 
+const addViewUid = (uid: number, command: readonly any[]): readonly any[] => {
+  if (command[0] === 'Viewlet.focusSelector') {
+    return [command[0], uid, ...command.slice(1)]
+  }
+  return command
+}
+
 export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] | Promise<readonly any[]> => {
   const { newState, oldState } = SourceControlStates.get(uid)
   SourceControlStates.set(uid, newState, newState)
@@ -12,7 +19,7 @@ export const render2 = (uid: number, diffResult: readonly number[]): readonly an
 
 const renderDirect = async (uid: number, commands: readonly any[]): Promise<readonly any[]> => {
   const rendererWorkerCommands = commands.filter((command) => command[0] === 'Viewlet.setFocusContext')
-  const rendererProcessCommands = commands.filter((command) => command[0] !== 'Viewlet.setFocusContext')
+  const rendererProcessCommands = commands.filter((command) => command[0] !== 'Viewlet.setFocusContext').map((command) => addViewUid(uid, command))
   const transactionId = await RendererProcess.invoke('Viewlet.queueCommands', uid, rendererProcessCommands)
   return [...rendererWorkerCommands, ['Viewlet.commitPending', uid, transactionId]]
 }
